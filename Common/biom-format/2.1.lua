257a96b6f97a5a1fd63b2a692611501b731bc219
Adam Caprez <acaprez2@unl.edu>
2018-05-16 16:46:54 -0500

    Fix biom-format version number.

--- Common/biom-format/2.1.lua
+++ Common/biom-format/2.1.lua
@@ -12,6 +12,6 @@ whatis("Keywords: Biology, Genomics, Sequencing")
 whatis("Description: The BIOM file format (canonically pronounced biome) is designed to be a general-use format for representing biological sample by observation contingency tables. BIOM is a recognized standard for the Earth Microbiome Project and is a Genomics Standards Consortium supported project.")
 whatis("URL: http://biom-format.org")
 
-prepend_path{"PATH"		,"/util/opt/anaconda/deployed-conda-envs/packages/biom-format/envs/biom-format-2.1.5/bin",priority=100}
-pushenv("CONDA_DEFAULT_ENV"	,"biom-format-2.1.5")
+prepend_path{"PATH"		,"/util/opt/anaconda/deployed-conda-envs/packages/biom-format/envs/biom-format-2.1.6/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"biom-format-2.1.6")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/biom-format/envs")

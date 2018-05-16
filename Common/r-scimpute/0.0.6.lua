b0ae1475fe4c6c4fdb5fb640e26c8c3276d5f361
Adam Caprez <acaprez2@unl.edu>
2018-05-16 17:35:38 -0500

    Fix r-scimpute version number.

--- Common/r-scimpute/0.0.6.lua
+++ Common/r-scimpute/0.0.6.lua
@@ -1,17 +1,17 @@
 help (
 [[
 This module loads r-scImpute.
-Version 0.0.5
+Version 0.0.6
 
 ]])
 
 whatis("Name: r-scImpute")
-whatis("Version: 0.0.5")
+whatis("Version: 0.0.6")
 whatis("Category: biology, scRNA-seq, normalization")
 whatis("Keywords: Biology, scRNA-sea, Gene Expression Analysis")
 whatis("URL: https://github.com/Vivianstats/scImpute")
 whatis("Description: scImpute is accurate and robust imputation of single-cell RNA sequencing data.")
 
-pushenv("CONDA_DEFAULT_ENV","r-scimpute-0.0.5")
-prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/r-scimpute/envs/r-scimpute-0.0.5/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV","r-scimpute-0.0.6")
+prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/r-scimpute/envs/r-scimpute-0.0.6/bin",priority=100}
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-scimpute/envs")

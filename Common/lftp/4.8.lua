bcf365faa586afdf8a61c41f45d944a45334ad19
Adam Caprez <acaprez2@unl.edu>
2018-05-16 17:26:24 -0500

    Fix lftp version number.

--- Common/lftp/4.8.lua
+++ Common/lftp/4.8.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads LFTP.
-Version 4.8.2
+Version 4.8.3
 ]]
 )
 
 whatis("Name: lftp")
-whatis("Version: 4.8.2")
+whatis("Version: 4.8.3")
 whatis("Category: system, utility")
 whatis("Keywords: System, Utility")
 whatis("URL: http://lftp.yar.ru")
 whatis("Description: LFTP is a sophisticated file transfer program supporting a number of network protocols (ftp, http, sftp, fish, torrent).")
 
-pushenv("CONDA_DEFAULT_ENV","lftp-4.8.2")
-prepend_path("PATH","/util/opt/anaconda/deployed-conda-envs/packages/lftp/envs/lftp-4.8.2/bin")
+pushenv("CONDA_DEFAULT_ENV","lftp-4.8.3")
+prepend_path("PATH","/util/opt/anaconda/deployed-conda-envs/packages/lftp/envs/lftp-4.8.3/bin")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/lftp/envs")

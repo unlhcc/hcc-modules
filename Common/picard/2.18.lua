c2725f99cbf1502f88525d1fe6112c5b9afa84f8
Adam Caprez <acaprez2@unl.edu>
2018-05-16 17:27:15 -0500

    Fix picard version number.

--- Common/picard/2.18.lua
+++ Common/picard/2.18.lua
@@ -1,7 +1,7 @@
 help (
 [[
 This module loads Picard.
-Version 2.18.2
+Version 2.18.4
 ]])
 
 if mode() == "load" then
@@ -9,12 +9,12 @@ if mode() == "load" then
 end
 
 whatis("Name: Picard")
-whatis("Version: 2.18.2")
+whatis("Version: 2.18.4")
 whatis("Category: computational biology, genomics")
 whatis("Keywords: Biology, Genomics, Alignment, Sequencing, File processing")
 whatis("URL: http://broadinstitute.github.io/picard/")
 whatis("Description: A set of Java command line tools for manipulating high-throughput sequencing data (HTS) data and formats. ")
 
-prepend_path{"PATH", "/util/opt/anaconda/deployed-conda-envs/packages/picard/envs/picard-2.18.2/bin", priority=100}
-pushenv("CONDA_DEFAULT_ENV", "picard-2.18.2")
+prepend_path{"PATH", "/util/opt/anaconda/deployed-conda-envs/packages/picard/envs/picard-2.18.4/bin", priority=100}
+pushenv("CONDA_DEFAULT_ENV", "picard-2.18.4")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/picard/envs")

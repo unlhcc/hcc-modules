38621969c6a84ba3f1c7b23a9bc469ff4bbb4b49
Adam Caprez <acaprez2@unl.edu>
2018-05-16 17:34:37 -0500

    Fix quasr path.

--- Common/quasr/6.09.lua
+++ Common/quasr/6.09.lua
@@ -11,6 +11,6 @@ whatis("Keywords: Computational Biology, NGS, Quality Assessment")
 whatis("Description: QUASR is lightweight pipeline for processing and analysis of high-throughput sequencing data.")
 whatis("URL: https://github.com/simonjwatson/QUASR/")
 
-pushenv("CONDA_DEFAULT_ENV","quasr-6.09")
-prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/quasr/envs/quasr-6.09/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV","quasr-v6.09")
+prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/quasr/envs/quasr-v6.09/bin",priority=100}
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/quasr/envs")

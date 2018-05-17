5a1b6af81efac4c79375e1661e4f6858492976d9
Adam Caprez <acaprez2@unl.edu>
2018-05-17 11:40:27 -0500

    Fix snp-pipeline modules.

--- Common/snp-pipeline/0.7.lua
+++ Common/snp-pipeline/0.7.lua
@@ -2,18 +2,18 @@ help (
 [[
 This module loads SNP Pipeline
 PATH has been prepended to include the SNP Pipeline program.
-Version 0.6.
+Version 0.7.
 ]])
 
 whatis("Name: SNP Pipeline")
-whatis("Version: 0.6")
+whatis("Version: 0.7")
 whatis("Category: computational biology, sequencing")
 whatis("Keywords: Biology, Genomics, Sequencing")
 whatis("Description: SNP Pipeline is a pipeline for the production of SNP matrices from sequence data used in the phylogenetic analysis of pathogenic organisms sequenced from samples of interest to food safety.")
 whatis("URL: http://snp-pipeline.readthedocs.io")
 
-prepend_path{"PATH"		,"/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs/snp-pipeline-0.6.1/bin",priority=100}
-pushenv("CONDA_DEFAULT_ENV"	,"snp-pipeline-0.6.1")
-pushenv("CLASSPATH",		"/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs/snp-pipeline-0.6.1/share/varscan-2.4.1-0/VarScan.jar")
+prepend_path{"PATH"		,"/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs/snp-pipeline-0.7.0/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"snp-pipeline-0.7.0")
+pushenv("CLASSPATH",		"/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs/snp-pipeline-0.7.0/share/varscan-2.4.3-0/VarScan.jar")
 setenv("PYTHON_EGG_CACHE",pathJoin(os.getenv("WORK"),".python-eggs"))
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs")
---
+++ Common/snp-pipeline/1.0.lua
@@ -0,0 +1,19 @@
+help (
+[[
+This module loads SNP Pipeline
+PATH has been prepended to include the SNP Pipeline program.
+Version 1.0.1
+]])
+
+whatis("Name: SNP Pipeline")
+whatis("Version: 1.0.1")
+whatis("Category: computational biology, sequencing")
+whatis("Keywords: Biology, Genomics, Sequencing")
+whatis("Description: SNP Pipeline is a pipeline for the production of SNP matrices from sequence data used in the phylogenetic analysis of pathogenic organisms sequenced from samples of interest to food safety.")
+whatis("URL: http://snp-pipeline.readthedocs.io")
+
+prepend_path{"PATH"		,"/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs/snp-pipeline-1.0.1/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"snp-pipeline-1.0.1")
+pushenv("CLASSPATH",		"/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs/snp-pipeline-1.0.1/share/varscan-2.4.3-0/VarScan.jar"
+setenv("PYTHON_EGG_CACHE",pathJoin(os.getenv("WORK"),".python-eggs"))
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snp-pipeline/envs")

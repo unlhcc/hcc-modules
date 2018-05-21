95a51225e5ae950f46d2cada463c4ab1e45a2330
Adam Caprez <acaprez2@unl.edu>
2018-05-21 21:02:11 +0000

    Add module files for augustus 3.2 and 3.3.

---
+++ Common/augustus/3.3.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Augustus.
+Version 3.3
+]]
+)
+whatis("Name: Augustus")
+whatis("Version: 3.3")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: http://bioinf.uni-greifswald.de/augustus")
+whatis("Description: AUGUSTUS is a gene prediction program for eukaryotes written by Mario Stanke and Oliver Keller. It can be used as an ab initio program, which means it bases its prediction purely on the sequence. AUGUSTUS may also incorporate hints on the gene structure coming from extrinsic sources such as EST, MS/MS, protein alignments and synthenic genomic alignments.")
+
+pushenv("CONDA_DEFAULT_ENV", "augustus-3.3")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/augustus/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/augustus/envs/augustus-3.3/bin")

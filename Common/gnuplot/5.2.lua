120390e0f740bfebeb7b9c98bec4fff57911ba32
Adam Caprez <acaprez2@unl.edu>
2018-05-24 20:05:20 +0000

    Add module fils for gnuplot 4.6 and 5.2.

---
+++ Common/gnuplot/5.2.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Gnuplot.
+Version 5.2.3
+]]
+)
+whatis("Name: Gnuplot")
+whatis("Version: 5.2.3")
+whatis("Category: Application")
+whatis("Keywords:  Application, Graphing")
+whatis("URL: http://www.gnuplot.info")
+whatis("Description: Gnuplot, plotting from command line")
+
+pushenv("CONDA_DEFAULT_ENV", "gnuplot-5.2.3")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gnuplot/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gnuplot/envs/gnuplot-5.2.3/bin")

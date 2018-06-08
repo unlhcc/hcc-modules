6228c8a876515a780b301faa533c10134031f049
Adam Caprez <acaprez2@unl.edu>
2018-06-08 19:24:05 +0000

    Add matlab module files.

---
+++ Common/matlab/r2014b.lua
@@ -0,0 +1,12 @@
+local help_message = [[
+MATLAB r2014b
+]]
+
+help(help_message,"\n")
+
+whatis("Name: MATLAB r2014b")
+whatis("Keywords: Application, Engineering, Math")
+
+setenv("MATLAB_PREFDIR", "/tmp/matlab." .. os.getenv("USER") .. "/prefs")
+
+prepend_path("PATH","/util/opt/matlab/r2014b/bin")

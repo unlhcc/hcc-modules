224c0e801473c9bc6451f46d21638727c3a712e9
Adam Caprez <acaprez2@unl.edu>
2018-11-16 22:28:43 +0000

    Add module files for gurobi-python 8.1.

---
+++ Common/gurobi-python/py36/8.1.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads Gurobi-python.
+Version 8.1.0
+]]
+)
+whatis("Name: Gurobi-python")
+whatis("Version: 8.1.0")
+whatis("Category: Mathematics, Optimization")
+whatis("Keywords: Mathematics, Optimization")
+whatis("URL: http://www.gurobi.com")
+whatis("Description: The Gurobi Optimizer is the state-of-the-art math programming solver able to handle all major problem types.")
+
+pushenv("CONDA_DEFAULT_ENV", "gurobi-8.1.0-py36")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi-python/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi-python/envs/gurobi-8.1.0-py36/bin")
+setenv("GUROBI_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi-python/envs/gurobi-8.1.0-py36")
+setenv("GRB_LICENSE_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi-python/gurobi.lic")

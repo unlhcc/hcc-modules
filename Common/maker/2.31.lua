fe88bb77a4c0c6fe284f19c161213e474e018713
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-12-20 17:54:52 -0600

    Update module files for maker and prokka

--- Common/maker/2.31.lua
+++ Common/maker/2.31.lua
@@ -2,22 +2,22 @@ help(
 [[
 MAKER is a portable and easily configurable genome annotation pipeline. Its purpose is to allow smaller eukaryotic and prokaryotic genome projects to independently annotate their genomes and to create genome databases. MAKER identifies repeats, aligns ESTs and proteins to a genome, produces ab-initio gene predictions and automatically synthesizes these data into gene annotations having evidence-based quality values.
 
-Version 2.31.9
+Version 2.31.10
 ]]
 )
 
 whatis("Name: MAKER")
-whatis("Version: 2.31.9")
+whatis("Version: 2.31.10")
 whatis("Category: computational biology, genomics")
 whatis("Keywords: Biology, Genomics, Gene Prediction, Genome Annotation")
 whatis("URL: http://www.yandell-lab.org/software/maker.html")
 whatis("Description: MAKER is a genome annotation pipeline for prokaryotic and smaller eukaryotic organisms.")
 
-prepend_path("PATH",                    "/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.9/sbin")
-prepend_path("PATH",			"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.9/bin")
-prepend_path("LD_LIBRARY_PATH",		"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.9/lib")
-prepend_path("INCLUDE",			"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.9/include")
-prepend_path("MANPATH",			"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.9/share/man")
-pushenv("CONDA_DEFAULT_ENV",		"maker-2.31.9")
+prepend_path("PATH",                    "/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.10/sbin")
+prepend_path("PATH",			"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.10/bin")
+prepend_path("LD_LIBRARY_PATH",		"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.10/lib")
+prepend_path("INCLUDE",			"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.10/include")
+prepend_path("MANPATH",			"/util/opt/anaconda/deployed-conda-envs/packages/maker/envs/maker-2.31.10/share/man")
+pushenv("CONDA_DEFAULT_ENV",		"maker-2.31.10")
 
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/maker/envs")

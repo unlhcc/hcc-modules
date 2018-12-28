01129cd0da1b52b6c67d2a475160570b282dc9a3
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-12-27 18:51:24 -0600

    Add module files for spades 3.13.0

---
+++ Common/spades/py27/3.13.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads SPAdes.
+To run on a test data set, use:
+spades.py --test
+Version 3.13.0
+]]
+)
+
+whatis("Name: Spades")
+whatis("Version: 3.13.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Assembly")
+whatis("URL: http://cab.spbu.ru/software/spades/")
+whatis("Description: SPAdes - St. Petersburg genome assembler - is intended for both standard isolates and single-cell MDA bacteria assemblies.")
+
+pushenv("CONDA_DEFAULT_ENV","spades-3.13.0-py27")
+prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/spades/envs/spades-3.13.0-py27/bin",priority=100}
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/spades/envs")

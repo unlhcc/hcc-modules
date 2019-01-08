0227f6723d308eecbccb47a2ff0122bec1acdcb8
Adam Caprez <acaprez2@unl.edu>
2019-01-08 21:49:01 +0000

    Add module file for mothur 1.41.

---
+++ Common/mothur/1.41.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Mothur.
+Version 1.41.0
+]]
+)
+whatis("Name: Mothur")
+whatis("Version: 1.41.0")
+whatis("Category: Bioinformatics")
+whatis("Keywords: Bioinformatics")
+whatis("URL: https://www.mothur.org")
+whatis("Description: This project seeks to develop a single piece of open-source, expandable software to fill the bioinformatics needs of the microbial ecology community.")
+
+pushenv("CONDA_DEFAULT_ENV", "mothur-1.41.0")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mothur/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mothur/envs/mothur-1.41.0/bin")

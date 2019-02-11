5d9ca59962b2808849d61621d352dad1bd8c9b25
Adam Caprez <acaprez2@unl.edu>
2019-02-11 21:33:37 +0000

    Add module file for ViSit 2.10.

---
+++ Common/visit/2.10.lua
@@ -0,0 +1,15 @@
+help(
+[[
+This module loads Visit.
+Version 2.10.2
+]]
+)
+
+whatis("Name: visit")
+whatis("Version: 2.10.2")
+whatis("Category: utility, visualization")
+whatis("Keywords: Utility, Visualization")
+whatis("URL: http://visit.llnl.gov")
+whatis("Description: VisIt is an Open Source, interactive, scalable, visualization, animation and analysis tool.")
+
+prepend_path("PATH","/util/opt/visit/2.10/bin")

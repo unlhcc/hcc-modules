de466d909456652b86b0abf1f22af2611cd3e13d
Adam Caprez <acaprez2@unl.edu>
2019-03-25 21:32:06 +0000

    Add anaconda modules.

---
+++ Common/anaconda/3.3.lua
@@ -0,0 +1,39 @@
+local help_message = [[
+Python 3.3
+
+This module loads the Anaconda Python environment. 
+See https://store.continuum.io/cshop/anaconda.
+
+Commonly used installed packages include:
+Numpy
+Scipy
+Matplotlib
+BioPython
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: Python")
+whatis("Version: 3.3")
+whatis("Category: system, development")
+whatis("Keywords: System, Development, Python")
+whatis("URL: http://www.python.org/")
+
+prepend_path("PATH",                "/util/opt/anaconda3/2.0/bin")
+prepend_path("MANPATH",             "/util/opt/anaconda3/2.0/share/man")
+prepend_path{"PATH",                "/util/opt/anaconda3/2.0/envs/py33/bin",priority=100}
+
+local username = os.getenv("USER") or ""
+if username ~= "root" then
+  prepend_path("CONDA_ENVS_PATH", "~/.conda/envs")
+
+  local group_conda_env = pathJoin(os.getenv("CONDA_GROUP_ENV"),"3") or ""
+  if isDir(group_conda_env) then
+    append_path("CONDA_ENVS_PATH",group_conda_env)
+  end
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda3/2.0/envs")
+end
+
+pushenv("CONDA_DEFAULT_ENV",	    "py33")
+family("python")
---
+++ Common/anaconda/4.3.lua
@@ -0,0 +1,38 @@
+local help_message = [[
+Anaconda 4.3
+
+This module loads the Anaconda environment. 
+See https://store.continuum.io/cshop/anaconda.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: Anaconda")
+whatis("Version: 4.3")
+whatis("Category: system, development")
+whatis("Keywords: System, Development, Python")
+whatis("URL: https://www.continuum.io/anaconda-overview")
+
+prepend_path("PATH",                "/util/opt/anaconda/4.3/bin")
+prepend_path("MANPATH",             "/util/opt/anaconda/4.3/share/man")
+
+local username = os.getenv("USER") or ""
+if username ~= "root" then
+  prepend_path("CONDA_ENVS_PATH", "~/.conda/envs")
+
+  local group_conda_env = pathJoin(os.getenv("CONDA_GROUP_ENV"),"3") or ""
+  if isDir(group_conda_env) then
+    append_path("CONDA_ENVS_PATH",group_conda_env)
+  end
+
+  local group_conda_env = pathJoin(os.getenv("CONDA_GROUP_ENV"),"2") or ""
+  if isDir(group_conda_env) then
+    append_path("CONDA_ENVS_PATH",group_conda_env)
+  end
+
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda3/2.0/envs")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/2.2/envs")
+end
+
+family("python")

367781ee44aad456b711600e02d11cbe7568d9f4
Adam Caprez <acaprez2@unl.edu>
2019-04-01 22:31:05 +0000

    Fix path.  yeesh.

--- Common/r-asreml/r34/4.1.lua
+++ Common/r-asreml/r34/4.1.lua
@@ -14,7 +14,7 @@ whatis("URL: https://www.vsni.co.uk/software/asreml-r")
 whatis("Description: ASReml-R is a statistical package that fits linear mixed models using Residual Maximum Likelihood (REML) in the R environment.")
 
 pushenv("CONDA_DEFAULT_ENV","mro-asreml-4.1.0.98-r34")
-prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/mro-asreml/envs/mro-asreml-4.1.0.98-r34",priority=100}
+prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/mro-asreml/envs/mro-asreml-4.1.0.98-r34/bin",priority=100}
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mro-asreml/envs")
 family("R")
 setenv("ASREML_LICENSE_FILE","/util/opt/asreml/4.1/bin/asreml.lic")

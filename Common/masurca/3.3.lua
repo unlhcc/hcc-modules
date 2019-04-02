14c58c453e2e0acd2db858b8f0808ff5fae04160
Adam Caprez <acaprez2@unl.edu>
2019-04-02 22:28:13 +0000

    Fix description line.

--- Common/masurca/3.3.lua
+++ Common/masurca/3.3.lua
@@ -9,10 +9,7 @@ whatis("Version: 3.3.1")
 whatis("Category: Biology, Genomics, Assembly")
 whatis("Keywords: Biology, Genomics, Assembly")
 whatis("URL: http://masurca.blogspot.co.uk/")
-whatis("Description: MaSuRCA (Maryland Super-Read Celera Assembler) genome assembly software.
-MaSuRCA requires Illumina data, and supports third-generation PacBio/Nanopore
-MinION reads for hybrid assembly.
-")
+whatis("Description: MaSuRCA (Maryland Super-Read Celera Assembler) genome assembly software. MaSuRCA requires Illumina data, and supports third-generation PacBio/Nanopore MinION reads for hybrid assembly.")
 
 pushenv("CONDA_DEFAULT_ENV", "masurca-3.3.1")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/masurca/envs")

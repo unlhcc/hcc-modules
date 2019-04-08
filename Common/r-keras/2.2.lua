53849fc96a464ca0971e7666ca6a7c50d69d02a7
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-04-08 20:17:40 +0000

    Add module file for r-keras

---
+++ Common/r-keras/2.2.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads r-keras.
+Version 2.2.4.1
+]]
+)
+
+whatis("Name: r-keras")
+whatis("Version: 2.2.4.1")
+whatis("Category: machine learning")
+whatis("Keywords: machine learning, mathematics, r")
+whatis("URL: https://keras.rstudio.com")
+whatis("Description: Interface to 'Keras' <https://keras.io>, a high-level neural networks 'API'.")
+
+pushenv("CONDA_DEFAULT_ENV", "r-keras-2.2.4.1")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-keras/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-keras/envs/r-keras-2.2.4.1/bin")

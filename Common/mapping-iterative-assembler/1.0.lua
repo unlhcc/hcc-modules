0895daca91844537627d53ae71de75d40db5b820
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-04-13 00:20:03 +0000

    Add module file for mia

---
+++ Common/mapping-iterative-assembler/1.0.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads mapping-iterative-assembler.
+Version 1.0
+]]
+)
+
+whatis("Name: mapping-iterative-assembler")
+whatis("Version: 1.0")
+whatis("Category: bioinformatics")
+whatis("Keywords: bioinformatics, assembly, mitochondria")
+whatis("URL: https://github.com/mpieva/mapping-iterative-assembler")
+whatis("Description: Consensus calling or `reference assisted assembly`, chiefly of ancient mitochondria.")
+
+pushenv("CONDA_DEFAULT_ENV", "mapping-iterative-assembler-1.0")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mapping-iterative-assembler/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mapping-iterative-assembler/envs/mapping-iterative-assembler-1.0/bin")

3af4f7dc0736d52b330311d1567aa5186db8e64c
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-04-18 17:34:22 +0000

    Add module file for shovill

---
+++ Common/shovill/1.0.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Shovill.
+Version 1.0.4
+]]
+)
+
+whatis("Name: Shovill")
+whatis("Version: 1.0.4")
+whatis("Category: computational biology")
+whatis("Keywords: computational biology, genomics, assembly")
+whatis("URL: https://github.com/tseemann/shovill")
+whatis("Description: Microbial assembly pipeline for Illumina paired-end reads")
+
+pushenv("CONDA_DEFAULT_ENV", "shovill-1.0.4")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/shovill/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/shovill/envs/shovill-1.0.4/bin")

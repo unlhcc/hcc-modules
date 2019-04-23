ae0f224615d1a6ab03c150ff3c38a573b6c600e5
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-04-23 23:03:22 +0000

    Add module files for twobittofa and fatotwobit

---
+++ Common/ucsc-fatotwobit/377.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads ucsc-fatotwobit.
+Version 377
+]]
+)
+
+whatis("Name: ucsc-fatotwobit")
+whatis("Version: 377")
+whatis("Category: computational biology")
+whatis("Keywords: computational biology, fasta, 2bit")
+whatis("URL: http://hgdownload.cse.ucsc.edu/admin/exe/")
+whatis("Description: Convert DNA from fasta to 2bit format")
+
+pushenv("CONDA_DEFAULT_ENV", "ucsc-fatotwobit-377")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-fatotwobit/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-fatotwobit/envs/ucsc-fatotwobit-377/bin")
---
+++ Common/ucsc-twobittofa/357.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads ucsc-twobittofa.
+Version 357
+]]
+)
+
+whatis("Name: ucsc-twobittofa")
+whatis("Version: 357")
+whatis("Category: computational biology")
+whatis("Keywords: computational biology, 2bit, fasta")
+whatis("URL: http://hgdownload.cse.ucsc.edu/admin/exe/")
+whatis("Description: Convert all or part of .2bit file to fasta")
+
+pushenv("CONDA_DEFAULT_ENV", "ucsc-twobittofa-v357-1")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-twobittofa/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-twobittofa/envs/ucsc-twobittofa-v357-1/bin")

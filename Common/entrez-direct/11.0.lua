338b7b4a6ec53e3ebd75096ae8d9ae0c4454595b
Natasha Pavlovikj <npavlovikj2@unl.edu>
2019-06-17 06:52:28 +0000

    Add few module files

--- Common/entrez-direct/11.0.lua
+++ Common/entrez-direct/11.0.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads entrez-direct.
-Version 10.0
+Version 11.0
 ]]
 )
 
 whatis("Name: entrez-direct")
-whatis("Version: 10.0")
+whatis("Version: 11.0")
 whatis("Category: Entrez, Utilities")
 whatis("Keywords: Entrez, Utilities")
 whatis("URL: https://www.ncbi.nlm.nih.gov/books/NBK25501")
 whatis("Description: Entrez Direct (EDirect) is an advanced method for accessing the NCBI's set of interconnected databases (publication, sequence, structure, gene, variation, expression, etc.) from a UNIX terminal window.")
 
-pushenv("CONDA_DEFAULT_ENV", "entrez-direct-10.0.20180927")
+pushenv("CONDA_DEFAULT_ENV", "entrez-direct-11.0")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/entrez-direct/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/entrez-direct/envs/entrez-direct-10.0.20180927/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/entrez-direct/envs/entrez-direct-11.0/bin")
---
+++ Common/stacks/2.4.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Stacks.
+Version 2.4
+]]
+)
+
+whatis("Name: Stacks")
+whatis("Version: 2.4")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: computational biology, RAD-seq, genetic maps, genomics, phylogeography")
+whatis("URL: http://catchenlab.life.illinois.edu/stacks/")
+whatis("Description: Stacks is a software pipeline for building loci from RAD-seq")
+
+pushenv("CONDA_DEFAULT_ENV", "stacks-2.4")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/stacks/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/stacks/envs/stacks-2.4/bin")

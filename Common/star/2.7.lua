a52fd2d3ae941e5b2beceb5911d7ad651f4eb103
Adam Caprez <acaprez2@unl.edu>
2019-07-22 23:17:09 +0000

    Add module file for STAR 2.7.

---
+++ Common/star/2.7.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Star.
+Version 2.7.1a
+]]
+)
+whatis("Name: Star")
+whatis("Version: 2.7.1a")
+whatis("Category: biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: https://github.com/alexdobin/STAR")
+whatis("Description: An RNA-seq read aligner.")
+
+pushenv("CONDA_DEFAULT_ENV", "star-2.7.1a")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/star/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/star/envs/star-2.7.1a/bin")

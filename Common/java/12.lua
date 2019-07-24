0a569ef035f877acbad449f89bf60c5e7e3a1253
Adam Caprez <acaprez2@unl.edu>
2019-07-24 20:31:10 +0000

    Add module files for Java 11 and 12.

---
+++ Common/java/12.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads Java.
+
+Version 12
+]])
+
+whatis("Name: java")
+whatis("Version: 12")
+whatis("Category: system, compiler")
+whatis("Keywords:  system, utility, compiler")
+whatis("Description: Java JDK.")
+whatis("URL: http://www.java.com")
+
+prepend_path("PATH"       ,"/util/opt/java/12/bin")
+setenv("JAVA_HOME"	  ,"/util/opt/java/12")

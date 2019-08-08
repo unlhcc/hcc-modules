c8e7d2c2082c8992609b83bda3bfd3dc88ab85e9
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-08-08 18:52:29 +0000

    Add module file for snp-sites 2.4.1

---
+++ Common/snp-sites/2.4.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads SNP-sites.
+Version 2.4.1
+]]
+)
+
+whatis("Name: SNP-sites")
+whatis("Version: 2.4.1")
+whatis("Category: computational biology, alignment, SNPs")
+whatis("Keywords:  Biology, Alignment, SNP")
+whatis("Description: SNP-sites rapidly extracts SNPs from a multi-FASTA alignment.")
+whatis("URL: https://github.com/sanger-pathogens/snp-sites")
+
+pushenv("CONDA_DEFAULT_ENV", "snp-sites-2.4.1")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snp-sites/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snp-sites/envs/snp-sites-2.4.1/bin")

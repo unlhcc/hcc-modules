bcaf98b5fca29cb89041ce32309dc3617130cba0
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-08-09 08:38:52 +0000

    Add module file for r-rhierbaps

---
+++ Common/r-rhierbaps/1.1.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads r-rhierbaps.
+Version 1.1.2
+]]
+)
+
+whatis("Name: r-rhierbaps")
+whatis("Version: 1.1.2")
+whatis("Category: computational biology")
+whatis("Keywords: computational biology, population genomics, clustering")
+whatis("URL: https://github.com/gtonkinhill/rhierbaps")
+whatis("Description: rhierbaps is R implementation of hierBAPS.")
+
+pushenv("CONDA_DEFAULT_ENV", "r-rhierbaps-1.1.2")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-rhierbaps/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-rhierbaps/envs/r-rhierbaps-1.1.2/bin")

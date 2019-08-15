fd536ef4ae3f9db65e1166b3a2378a17e5c78087
Adam Caprez <acaprez2@unl.edu>
2019-08-15 00:01:24 +0000

    Add module files for gensim 3.8.

---
+++ Common/gensim/py37/3.8.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Gensim.
+Version 3.8.0
+]]
+)
+whatis("Name: Gensim")
+whatis("Version: 3.8.0")
+whatis("Category: Machine learning, topic modeling")
+whatis("Keywords: Machine learning, topic modeling")
+whatis("URL: http://github.com/RaRe-Technologies/gensim")
+whatis("Description: Topic Modelling for Humans")
+
+pushenv("CONDA_DEFAULT_ENV", "gensim-3.8.0-py37")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gensim/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gensim/envs/gensim-3.8.0-py37/bin")

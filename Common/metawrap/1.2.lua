9620c248eae2c229e37e78938b6ff69a4432aa54
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-09-06 23:21:57 +0000

    Add module file for metawrap 1.2

---
+++ Common/metawrap/1.2.lua
@@ -0,0 +1,21 @@
+help(
+[[
+This module loads metaWRAP.
+Version 1.2
+]]
+)
+
+if mode() == "load" then
+  LmodMessage("NOTE: The metaWRAP databases are already downloaded on HCC, and the config file, config-metawrap, is already set to point to these directories.")
+end
+
+whatis("Name: metaWRAP")
+whatis("Version: 1.2")
+whatis("Category: computational biology, metagenomics")
+whatis("Keywords: bioinformatics, metagenomic analysis, pipeline")
+whatis("URL: https://github.com/bxlab/metaWRAP")
+whatis("Description: MetaWRAP is a pipeline for genome-resolved metagenomic data analysis")
+
+pushenv("CONDA_DEFAULT_ENV", "metawrap-1.2")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/metawrap/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/metawrap/envs/metawrap-1.2/bin")

3276fb5fa32cd73d4d65f0076d91f314d96823f9
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-09-20 20:52:20 +0000

    Add module for r-matlib

---
+++ Common/r-matlib/0.9.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads r-matlib.
+Version 0.9.2
+]]
+)
+
+whatis("Name: r-matlib")
+whatis("Version: 0.9.2")
+whatis("Category: R, statistics, matrix")
+whatis("Keywords: R, statistics, matrix")
+whatis("URL: https://github.com/friendly/matlib")
+whatis("Description: A collection of matrix functions for teaching and learning matrix linear algebra as used in multivariate statistical methods.")
+
+pushenv("CONDA_DEFAULT_ENV", "r-matlib-0.9.2")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-matlib/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-matlib/envs/r-matlib-0.9.2/bin")

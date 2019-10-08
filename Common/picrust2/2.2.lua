2b6bea9ab773d4f0c80509a8d8fddba83bf4d723
Adam Caprez <acaprez2@unl.edu>
2019-10-08 18:13:41 +0000

    Add module file for picrust2 2.2.

---
+++ Common/picrust2/2.2.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Picrust2.
+Version 2.2.0_b
+]]
+)
+whatis("Name: Picrust2")
+whatis("Version: 2.2.0_b")
+whatis("Category: Pipeline, Phylogenetics")
+whatis("Keywords: Pipeline, Phylogenetics")
+whatis("URL: https://github.com/picrust/picrust2")
+whatis("Description: PICRUSt: Phylogenetic Investigation of Communities by Reconstruction of Unobserved States")
+
+pushenv("CONDA_DEFAULT_ENV", "picrust2-2.2.0_b")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/picrust2/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/picrust2/envs/picrust2-2.2.0_b/bin")

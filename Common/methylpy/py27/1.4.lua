695e93d257a979bdbd1c06665828e14da920f49c
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-11-01 16:29:42 +0000

    Add module files for methylpy

---
+++ Common/methylpy/py27/1.4.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads methylpy.
+Version 1.4.1
+]]
+)
+
+whatis("Name: methylpy")
+whatis("Version: 1.4.1")
+whatis("Category: bioinformatics, methylation, pipeline")
+whatis("Keywords: genomics, bisulfite sequencing, methylation analysis")
+whatis("URL: https://github.com/yupenghe/methylpy")
+whatis("Description: Bisulfite sequencing data processing and differential methylation analysis")
+
+pushenv("CONDA_DEFAULT_ENV", "methylpy-1.4.1-py27")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/methylpy/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/methylpy/envs/methylpy-1.4.1-py27/bin")

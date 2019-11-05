977249e7efb11f34b20312feac8485d163149cb8
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-11-05 22:15:07 +0000

    Add java message for snpeff

--- Common/snpeff/4.3.lua
+++ Common/snpeff/4.3.lua
@@ -7,6 +7,14 @@ To use snpEff, run the command "snpEff".
 To use SnpSift, run the command "SnpSift".
 ]])
 
+if mode() == "load" then
+  LmodMessage("-------------------------| Help message for snpEff module |-------------------------------")
+  LmodMessage("Usage: snpEff <command> <arguments>")
+  LmodMessage("You can also pass -Xms or -Xmx arguments to control Java memory allocation.")
+  LmodMessage("For example, to increase Java heap space to 10GB, run: snpEff -Xms512m -Xmx10g <command> <arguments>")
+  LmodMessage("------------------------------------------------------------------------------------------")
+end
+
 whatis("Name: snpeff")
 whatis("Version: 4.3")
 whatis("Category: computational biology, bioinformatics, data processing")

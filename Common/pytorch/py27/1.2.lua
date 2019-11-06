cb15c09dc3236ff13ab82f6d9edbe025b8347aef
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-11-06 03:03:48 +0000

    Add module files for pytorch 1.2.0

--- Common/pytorch/py27/1.2.lua
+++ Common/pytorch/py27/1.2.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads PyTorch.
-Version 0.4.1
+Version 1.2.0
 ]]
 )
 
 whatis("Name: PyTorch")
-whatis("Version: 0.4.1")
+whatis("Version: 1.2.0")
 whatis("Category: deep learning, machine learning, mathematics")
 whatis("Keywords: deep learning, machine learning, mathematics")
 whatis("URL: http://pytorch.org/")
 whatis("Description: PyTorch is an optimized tensor library for deep learning using GPUs and CPUs.")
 
-pushenv("CONDA_DEFAULT_ENV", "pytorch-0.4.1-py27")
+pushenv("CONDA_DEFAULT_ENV", "pytorch-1.2.0-py27")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch/envs/pytorch-0.4.1-py27/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch/envs/pytorch-1.2.0-py27/bin")

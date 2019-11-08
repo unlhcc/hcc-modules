5b2561e7e455d412497e8ba11e9eacfa4c2e2896
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-11-08 05:02:22 +0000

    Add module file for qgrs-cpp

---
+++ Common/qgrs-cpp/2017.08.25.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads qgrs-cpp.
+Version 2017.08.25
+]]
+)
+
+whatis("Name: qgrs-cpp")
+whatis("Version: 2017.08.25")
+whatis("Category: computational biology")
+whatis("Keywords: computational biology, mapping, qgrs")
+whatis("URL: https://github.com/freezer333/qgrs-cpp")
+whatis("Description: C++ implementation of QGRS mapping.")
+
+pushenv("CONDA_DEFAULT_ENV", "qgrs-cpp-2017.08.25")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/qgrs-cpp/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/qgrs-cpp/envs/qgrs-cpp-2017.08.25/bin")

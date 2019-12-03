6c5f07e37241f4c16049f0f36a53edea9e7ae43c
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2019-12-03 06:41:19 +0000

    Add module file for Keras 2.3.1

---
+++ Common/keras/py27/2.3.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Keras using the TensorFlow backend.
+Version 2.3.1
+]]
+)
+
+whatis("Name: Keras")
+whatis("Version: 2.3.1")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine Learning, Mathematics")
+whatis("URL: https://keras.io")
+whatis("Description: Deep Learning Library for Theano and TensorFlow")
+
+pushenv("CONDA_DEFAULT_ENV", "keras-2.3.1-py27")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras/envs/keras-2.3.1-py27/bin")

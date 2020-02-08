266c10009c4632e394476c29168348a57505a1f4
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2020-02-08 00:36:03 +0000

    Add and update module files

---
+++ Common/snp-dists/0.6.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads snp-dists.
+Version 0.6.3
+]]
+)
+
+whatis("Name: snp-dists")
+whatis("Version: 0.6.3")
+whatis("Category: computational biology, SNP")
+whatis("Keywords: computational biology, SNP, distance matrix")
+whatis("URL: https://github.com/tseemann/snp-dists")
+whatis("Description: Convert a FASTA alignment to SNP distance matrix")
+
+pushenv("CONDA_DEFAULT_ENV", "snp-dists-0.6.3")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snp-dists/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snp-dists/envs/snp-dists-0.6.3/bin")

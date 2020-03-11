7b01033f572903a5b5414b396e95912d95f42808
Adam Caprez <acaprez2@unl.edu>
2020-03-11 01:25:26 +0000

    Add module file for rust 1.41.

---
+++ Common/rust/1.41.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads Rust.
+Version 1.41.0
+]]
+)
+whatis("Name: Rust")
+whatis("Version: 1.41.0")
+whatis("Category: System, Compiler")
+whatis("Keywords: System, Compiler")
+whatis("URL: https://www.rust-lang.org")
+whatis("Description: Rust is a systems programming language that runs blazingly fast, prevents segfaults, and guarantees thread safety.
+This package provides the compiler (rustc) and the documentation utilities rustdoc.
+")
+
+pushenv("CONDA_DEFAULT_ENV", "rust-1.41.0")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rust/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rust/envs/rust-1.41.0/bin")

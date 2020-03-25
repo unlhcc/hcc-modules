cb288c94a41c6253492eadf0407a1c57b5225052
Adam Caprez <acaprez2@unl.edu>
2020-03-25 19:03:51 +0000

    Add module file for igs-portal-client 1.4.

---
+++ Common/igs-portal-client/1.4.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Igs-portal-client.
+Version 1.4.4
+]]
+)
+whatis("Name: Igs-portal-client")
+whatis("Version: 1.4.4")
+whatis("Category: System, Utility")
+whatis("Keywords: System, Utility")
+whatis("URL: https://github.com/IGS/portal_client")
+whatis("Description: Python-based client for downloading data made available through portals powered by the GDC-based portal system.")
+
+pushenv("CONDA_DEFAULT_ENV", "igs-portal-client-1.4.4")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/igs-portal-client/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/igs-portal-client/envs/igs-portal-client-1.4.4/bin")

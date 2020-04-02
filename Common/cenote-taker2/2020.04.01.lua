55a7c38e4eeba39e1b7af7f9683901165eaf2ec0
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2020-04-02 06:30:42 +0000

    Add module files for cenote-taker2 and vep

--- Common/cenote-taker2/2020.04.01.lua
+++ Common/cenote-taker2/2020.04.01.lua
@@ -1,20 +1,21 @@
 help(
 [[
 This module loads Cenote-Taker2.
-Version 2020.03.26
+Version 2020.04.01
 ]]
 )
 
 whatis("Name: Cenote-Taker2")
-whatis("Version: 2020.03.26")
+whatis("Version: 2020.04.01")
 whatis("Category: computational biology, viral genomes, pipeline")
 whatis("Keywords: computational biology, viral genomes, annotation")
 whatis("URL: https://github.com/mtisza1/Cenote-Taker2")
 whatis("Description: Cenote-Taker2 is a pipeline for divergent virus discovery and annotation.")
 
-prepend_path("PERL5LIB", "/util/opt/anaconda/deployed-conda-envs/packages/cenote-taker2/envs/cenote-taker2-2020.03.26/lib/site_perl/5.26.2/")
-setenv("CT2_DIR", "/work/HCC/BCRF/app_specific/cenote-taker2/2020.03.26/")
+load("singularity")
+prepend_path("PERL5LIB", "/util/opt/anaconda/deployed-conda-envs/packages/cenote-taker2/envs/cenote-taker2-2020.04.01/lib/site_perl/5.26.2/")
+setenv("CT2_DIR", "/work/HCC/BCRF/app_specific/cenote-taker2/2020.04.01/")
 
-pushenv("CONDA_DEFAULT_ENV", "cenote-taker2-2020.03.26")
+pushenv("CONDA_DEFAULT_ENV", "cenote-taker2-2020.04.01")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cenote-taker2/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cenote-taker2/envs/cenote-taker2-2020.03.26/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cenote-taker2/envs/cenote-taker2-2020.04.01/bin")

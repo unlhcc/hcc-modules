53312b71c770702335345f8c89df9e6b7dc2c57f
Natasha Pavlovikj <npavlovikj2@unl.edu>
2020-04-22 17:33:48 -0500

    Update checkm-genome module and set db var

--- Common/checkm-genome/1.1.lua
+++ Common/checkm-genome/1.1.lua
@@ -1,18 +1,18 @@
 help(
 [[
 This module loads CheckM Genome.
-Version 1.0.18
+Version 1.1.2
 ]]
 )
 
 whatis("Name: checkm-genome")
-whatis("Version: 1.0.18")
+whatis("Version: 1.1.2")
 whatis("Category: computational biology, quality assessment")
 whatis("Keywords: genomics, quality assessment, metagenomics")
 whatis("URL: https://ecogenomics.github.io/CheckM")
 whatis("Description: Assess the quality of microbial genomes recovered from isolates, single cells, and metagenomes.")
 
-setenv("CHECKM_ROOT_DIR", "/work/HCC/BCRF/app_specific/checkm_databases/1.0.18/")
-pushenv("CONDA_DEFAULT_ENV", "checkm-genome-1.0.18")
+setenv("CHECKM_ROOT_DIR", "/work/HCC/BCRF/app_specific/checkm_databases/1.1.2/")
+pushenv("CONDA_DEFAULT_ENV", "checkm-genome-1.1.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/checkm-genome/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/checkm-genome/envs/checkm-genome-1.0.18/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/checkm-genome/envs/checkm-genome-1.1.2/bin")

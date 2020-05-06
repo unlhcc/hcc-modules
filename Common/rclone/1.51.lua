131f6af1d338e14f519082bbc178f3147876aebd
Adam Caprez <acaprez2@unl.edu>
2020-05-06 21:40:25 +0000

    Add module file for rclone 1.51.

---
+++ Common/rclone/1.51.lua
@@ -0,0 +1,29 @@
+help(
+[[
+This module loads Rclone.
+Version 1.51.0
+]]
+)
+whatis("Name: Rclone")
+whatis("Version: 1.51.0")
+whatis("URL: https://rclone.org/")
+whatis("Category: Storage, Utilities")
+whatis("Keywords: Storage, Utilities")
+whatis("Description: Rclone ("rsync for cloud storage") is a command line program to
+sync files and directories to and from different cloud storage providers.
+")
+
+pushenv("CONDA_DEFAULT_ENV", "rclone-1.51.0")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rclone/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rclone/envs/rclone-1.51.0/bin")
+setenv("RCLONE_CONFIG",pathJoin(os.getenv("WORK"),".config/rclone/rclone.conf"))
+
+
+if mode() == "load" then
+  local w = os.getenv("WORK")
+  local d = pathJoin(w,".config/rclone")
+  if (not isDir(d)) then
+    lfs.mkdir(pathJoin(w,".config"))
+    lfs.mkdir(d)
+  end
+end

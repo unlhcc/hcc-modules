73469f0b07e5565faddac7fac4acdd4602af1aa0
Jean-Jack N00b Riethoven <jriethoven2@unl.edu>
2020-06-01 12:48:07 -0500

    Added QIIME2 2020.2

--- Common/qiime2/2020.2.lua
+++ Common/qiime2/2020.2.lua
@@ -13,12 +13,12 @@ whatis("URL: https://qiime2.org/")
 whatis("Description: QIIME 2 is a powerful, extensible, and decentralized microbiome analysis package with a focus on data and analysis transparency.")
 
 pushenv("CONDA_DEFAULT_ENV","qiime2-2020.2")
-prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/qiime2/envs/qiime2-2020.2/bin",priority=100}
+prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/qiime2/envs/qiime2-2020.2.0/bin",priority=100}
 
 setenv("MPLBACKEND","Agg")
 -- The q2cli package (via click) creates a cache on first run.  By default, this is ~/.config
 -- If the first run is on a worker, this will fail.  Setting this var tells it to cache to
 -- $WORK/.config instead.
 setenv("XDG_CONFIG_HOME",pathJoin(os.getenv("WORK"),".config"))
-execute {cmd="source /util/opt/anaconda/deployed-conda-envs/packages/qiime2/envs/qiime2-2020.2/bin/tab-qiime",modeA={"load"}}
+execute {cmd="source /util/opt/anaconda/deployed-conda-envs/packages/qiime2/envs/qiime2-2020.2.0/bin/tab-qiime",modeA={"load"}}
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/qiime2/envs")

b432ca99396f9948f1d09ae180a29d004e918f1a
Adam Caprez <acaprez2@unl.edu>
2020-06-18 01:35:24 +0000

    Add module file for r-inla 20.03.17.

---
+++ Common/r-inla/20.03.17.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads r-inla.
+Version 20.03.17
+]]
+)
+whatis("Name: r-inla")
+whatis("Version: 20.03.17")
+whatis("Category: Mathematics, Statistics")
+whatis("Keywords: Mathematics, Statistics")
+whatis("URL: http://www.r-inla.org/")
+whatis("Description: Full Bayesian Analysis of Latent Gaussian Models using Integrated Nested Laplace Approximations")
+
+pushenv("CONDA_DEFAULT_ENV", "r-inla-20.03.17")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-inla/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-inla/envs/r-inla-20.03.17/bin")

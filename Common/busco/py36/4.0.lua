ef84089eb99811448905f73d0cf9a39d025d840f
Natasha Pavlovikj <npavlovikj2@unl.edu>
2020-07-06 19:04:31 -0500

    Add module file for busco 4.0

--- Common/busco/py36/4.0.lua
+++ Common/busco/py36/4.0.lua
@@ -1,25 +1,25 @@
 help(
 [[
 This module loads BUSCO.
-Version 3.0.2
+Version 4.0.6
 ]]
 )
 
 if mode() == "load" then
   LmodMessage("Before you use BUSCO, you need to copy the configuration file and set the path to your data and ORTHODB files.")
   LmodMessage("Copy the config file, config.ini, to the current directory: cp $BUSCO_CONFIG_FILE config.ini")
-  LmodMessage("Check the available OrthoDB files: ls $ORTHODB_V9")
+  LmodMessage("Check the available OrthoDB files: ls $ORTHODB_V10")
 end
 
 whatis("Name: BUSCO")
-whatis("Version: 3.0.2")
+whatis("Version: 4.0.6")
 whatis("Category: computational biology, benchmarking")
 whatis("Keywords: genome assembly, assessment, annotation")
 whatis("URL: https://busco.ezlab.org/")
 whatis("Description: BUSCO provides measures for quantitative assessment of genome assembly, gene set, and transcriptome completeness based on evolutionarily informed expectations of gene content from near-universal single-copy orthologs selected from OrthoDB.")
 
 load("biodata/1.0")
-pushenv("CONDA_DEFAULT_ENV", "busco-3.0.2-py35")
+pushenv("CONDA_DEFAULT_ENV", "busco-4.0.6-py36")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-3.0.2-py35/bin")
-prepend_path("BUSCO_CONFIG_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-3.0.2-py35/config/config.ini")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-4.0.6-py36/bin")
+prepend_path("BUSCO_CONFIG_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-4.0.6-py36/config/config.ini")

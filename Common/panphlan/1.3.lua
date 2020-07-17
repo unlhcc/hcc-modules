0832a43c2ac71776f30ea906ba84eee2d4748d31
Adam Caprez <acaprez2@unl.edu>
2020-07-17 04:00:22 +0000

    Fix description.

--- Common/panphlan/1.3.lua
+++ Common/panphlan/1.3.lua
@@ -9,13 +9,7 @@ whatis("Version: 1.3")
 whatis("Category: biology, genomics")
 whatis("Keywords: biology, genomics")
 whatis("URL: https://bitbucket.org/CibioCM/panphlan")
-whatis("Description: PanPhlAn is a strain-level metagenomic profiling tool for identifying the
-gene composition and in-vivo transcriptional activity of individual strains
-in metagenomic samples. PanPhlAn's ability for strain-tracking and
-functional analysis of unknown pathogens makes it an efficient tool for
-culture-free infectious outbreak epidemiology and microbial population
-studies.
-")
+whatis("Description: PanPhlAn is a strain-level metagenomic profiling tool for identifying the gene composition and in-vivo transcriptional activity of individual strains in metagenomic samples.")
 
 pushenv("CONDA_DEFAULT_ENV", "panphlan-1.3")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/panphlan/envs")

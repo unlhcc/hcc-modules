69d10a44b973850433e76ae390cc023e49ce34d7
Adam Caprez <acaprez2@unl.edu>
2020-07-29 05:03:23 +0000

    Add module files for icount 2.0.

---
+++ Common/icount/py36/2.0.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Icount.
+Version 2.0.0
+]]
+)
+whatis("Name: Icount")
+whatis("Version: 2.0.0")
+whatis("Category: Bioinformatics, Iclip")
+whatis("Keywords: Bioinformatics, Iclip")
+whatis("URL: https://github.com/tomazc/iCount")
+whatis("Description: Computational pipeline for analysis of iCLIP data")
+
+pushenv("CONDA_DEFAULT_ENV", "icount-2.0.0-py36")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/icount/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/icount/envs/icount-2.0.0-py36/bin")
+family("python")

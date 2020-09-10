bfa8394f580a9051cd826766c7a5ce93eefaa26c
fedora Cloud User <fedora@tanash-fedora-32.novalocal>
2020-09-10 17:15:38 +0000

    Add module file for dedalus-2.1905

---
+++ Common/dedalus/py37/2.1905.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Dedalus.
+Version 2.1905
+]]
+)
+whatis("Name: Dedalus")
+whatis("Version: 2.1905")
+whatis("Category: partial differential equation, fluid dynamics")
+whatis("Keywords: partial differential equation, python, spectral methods")
+whatis("URL: https://dedalus-project.readthedocs.io/en/latest/")
+whatis("Description: A flexible framework for solving differential equations using spectral methods.")
+
+pushenv("CONDA_DEFAULT_ENV", "dedalus-2.1905-py37")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dedalus/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dedalus/envs/dedalus-2.1905-py37/bin")

ebf3884cd6daaae8738c7f40bdb05aff6df1e0f4
Natasha Pavlovikj <npavlovikj2@unl.edu>
2020-09-10 21:22:40 -0500

    Update module file for stringMLST

--- Common/stringmlst/0.6.lua
+++ Common/stringmlst/0.6.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads stringMLST.
-Version 0.6.2
+Version 0.6.3
 ]]
 )
 
 whatis("Name: stringMLST")
-whatis("Version: 0.6.2")
+whatis("Version: 0.6.3")
 whatis("Category: computational biology, genome")
 whatis("Keywords: MLST, k-mer, raw reads")
 whatis("URL: https://github.com/jordanlab/stringMLST")
 whatis("Description: Fast k-mer based tool for multi locus sequence typing (MLST) directly from genome sequencing reads")
 
-pushenv("CONDA_DEFAULT_ENV", "stringmlst-0.6.2")
+pushenv("CONDA_DEFAULT_ENV", "stringmlst-0.6.3")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/stringmlst/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/stringmlst/envs/stringmlst-0.6.2/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/stringmlst/envs/stringmlst-0.6.3/bin")

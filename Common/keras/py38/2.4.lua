74208d82e275173177462bb6eeb6faebcb7b2143
Adam Caprez <acaprez2@unl.edu>
2020-09-20 06:16:17 +0000

    Add module files for keras 2.4.

---
+++ Common/keras/py38/2.4.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Keras using the TensorFlow backend.
+Version 2.4.3
+]]
+)
+
+whatis("Name: Keras")
+whatis("Version: 2.4.3")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine Learning, Mathematics")
+whatis("URL: https://keras.io")
+whatis("Description: Deep Learning Library for Theano and TensorFlow")
+
+pushenv("CONDA_DEFAULT_ENV", "keras-2.4.3-py38")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras/envs/keras-2.4.3-py38/bin")

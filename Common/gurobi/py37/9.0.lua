4a87881aa294b6d6a2d7b12921813430a3f780ac
Adam Caprez <acaprez2@unl.edu>
2020-10-07 01:05:47 +0000

    Add module files for gurobi 9.0.

---
+++ Common/gurobi/py37/9.0.lua
@@ -0,0 +1,21 @@
+help(
+[[
+This module loads Gurobi.
+Version 9.0.3
+
+This module also adds the Gurobi MATLAB directory to MATLAB's startup path.
+]]
+)
+whatis("Name: Gurobi-python")
+whatis("Version: 9.0.3")
+whatis("Category: Mathematics, Optimization")
+whatis("Keywords: Mathematics, Optimization")
+whatis("URL: http://www.gurobi.com")
+whatis("Description: The Gurobi Optimizer is the state-of-the-art math programming solver able to handle all major problem types.")
+
+pushenv("CONDA_DEFAULT_ENV", "gurobi-9.0.3-py37")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-9.0.3-py37/bin")
+setenv("GUROBI_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-9.0.3-py37")
+setenv("GRB_LICENSE_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/gurobi.lic")
+prepend_path("MATLABPATH","/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-9.0.3-py37/share/gurobi-9.0.3-0/matlab")

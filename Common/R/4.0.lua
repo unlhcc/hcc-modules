db1e13a465fbf9269efaf2cf210138e16235c82b
Adam Caprez <acaprez2@unl.edu>
2020-12-02 03:07:48 +0000

    Update R 4.0 module to 4.0.3.

--- Common/R/4.0.lua
+++ Common/R/4.0.lua
@@ -5,15 +5,15 @@ Version 4.0
 ]]
 )
 whatis("Name: R")
-whatis("Version: 4.0.0")
+whatis("Version: 4.0.3")
 whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: http://www.r-project.org/")
 whatis("Description: R is a free software environment for statistical computing and graphics.")
 
-pushenv("CONDA_DEFAULT_ENV", "r-4.0.0")
+pushenv("CONDA_DEFAULT_ENV", "r-4.0.3")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.0.0/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.0.3/bin")
 
 family("R")
 

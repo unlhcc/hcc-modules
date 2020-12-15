a9a2a8edafe34e5f39a3b79b2e9f5e7de8421f3d
Adam Caprez <acaprez2@unl.edu>
2020-12-15 21:03:37 +0000

    Add module file for freesurfer 7.1.

---
+++ Common/freesurfer/7.1.lua
@@ -0,0 +1,35 @@
+help(
+[[
+This module loads FreeSurfer.
+Version 7.1.1
+]]
+)
+
+whatis("Name: FreeSurfer")
+whatis("Version: 7.1.1")
+whatis("Category: computational neuroimaging")
+whatis("Keywords: MRI, visualization, analysis")
+whatis("URL: https://surfer.nmr.mgh.harvard.edu/")
+whatis("Description: An open source software suite for processing and analyzing (human) brain MRI images.")
+
+setenv("FREESURFER_HOME", "/util/opt/freesurfer/7.1")
+setenv("FIX_VERTEX_AREA","")
+setenv("FMRI_ANALYSIS_DIR","/util/opt/freesurfer/7.1/fsfast")
+setenv("FREESURFER","/util/opt/freesurfer/7.1")
+setenv("FSFAST_HOME","/util/opt/freesurfer/7.1/fsfast")
+setenv("FSF_OUTPUT_FORMAT","nii.gz")
+setenv("FS_OVERRIDE","0")
+setenv("FUNCTIONALS_DIR","/util/opt/freesurfer/7.1/sessions")
+setenv("LOCAL_DIR","/util/opt/freesurfer/7.1/local")
+setenv("MINC_BIN_DIR","/util/opt/freesurfer/7.1/mni/bin")
+setenv("MINC_LIB_DIR","/util/opt/freesurfer/7.1/mni/lib")
+setenv("MNI_DATAPATH","/util/opt/freesurfer/7.1/mni/data")
+setenv("MNI_DIR","/util/opt/freesurfer/7.1/mni")
+setenv("MNI_PERL5LIB","/util/opt/freesurfer/7.1/mni/share/perl5")
+setenv("OS","Linux")
+prepend_path("PATH","/util/opt/freesurfer/7.1/mni/bin")
+prepend_path("PATH","/util/opt/freesurfer/7.1/tktools")
+prepend_path("PATH","/util/opt/freesurfer/7.1/fsfast/bin")
+prepend_path("PATH","/util/opt/freesurfer/7.1/bin")
+setenv("PERL5LIB","/util/opt/freesurfer/7.1/mni/share/perl5")
+setenv("SUBJECTS_DIR","/util/opt/freesurfer/7.1/subjects")

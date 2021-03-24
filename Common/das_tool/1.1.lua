dd35c131ed4bc2094b9d5269ddb44dd94de26f50
Adam Caprez <acaprez2@unl.edu>
2021-03-24 00:33:26 +0000

    Anaconda 4.9

--- Common/das_tool/1.1.lua
+++ Common/das_tool/1.1.lua
@@ -9,9 +9,7 @@ whatis("Version: 1.1.2")
 whatis("Category: Genomics, Metagenomics")
 whatis("Keywords: Genomics, Metagenomics")
 whatis("URL: https://github.com/cmks/DAS_Tool")
-whatis("Description: Recovery of genomes from metagenomes via a dereplication,
-aggregation and scoring strategy.
-")
+whatis("Description: Recovery of genomes from metagenomes via a dereplication,aggregation and scoring strategy.")
 
 pushenv("CONDA_DEFAULT_ENV", "das_tool-1.1.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/das_tool/envs")

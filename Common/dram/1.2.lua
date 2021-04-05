9dd7fb1507059048f2ee8792baf1075542c97be1
Natasha Pavlovikj <npavlovikj2@unl.edu>
2021-04-05 00:34:31 -0500

    Add module for dram 1.2.0

--- Common/dram/1.2.lua
+++ Common/dram/1.2.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads DRAM.
-Version 1.1.1
+Version 1.2.0
 ]]
 )
 
 whatis("Name: DRAM")
-whatis("Version: 1.1.1")
+whatis("Version: 1.2.0")
 whatis("Category: genomics, annotation")
 whatis("Keywords: genomics, metabolism, function")
 whatis("URL: https://github.com/shafferm/DRAM/")
 whatis("Description: Distilled and Refined Annotation of Metabolism: A tool for the annotation and curation of function for microbial and viral genomes")
 
-pushenv("CONDA_DEFAULT_ENV", "dram-1.1.1")
+pushenv("CONDA_DEFAULT_ENV", "dram-1.2.0")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dram/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dram/envs/dram-1.1.1/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dram/envs/dram-1.2.0/bin")

a0494298e9b8b798d95e25bcd8ff3a418d611c41
Natasha Pavlovikj <npavlovikj2@unl.edu>
2021-04-14 20:39:38 -0500

    Add module for spades 3.15

---
+++ Common/spades/3.15.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Spades.
+Version 3.15.2
+]]
+)
+whatis("Name: Spades")
+whatis("Version: 3.15.2")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Assembly")
+whatis("URL: http://cab.spbu.ru/software/spades/")
+whatis("Description: SPAdes - St. Petersburg genome assembler - is intended for both standard isolates and single-cell MDA bacteria assemblies.")
+
+pushenv("CONDA_DEFAULT_ENV", "spades-3.15.2")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/spades/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/spades/envs/spades-3.15.2/bin")

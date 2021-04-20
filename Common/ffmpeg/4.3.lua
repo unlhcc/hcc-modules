0ee35adaece91c82b9e06fc6565c8f9d81c5dea7
Adam Caprez <acaprez2@unl.edu>
2021-04-20 22:46:29 +0000

    Add module file for ffmpeg 4.3.

---
+++ Common/ffmpeg/4.3.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads ffmpeg.
+Version 4.3.1
+]]
+)
+whatis("Name: ffmpeg")
+whatis("Version: 4.3.1")
+whatis("Category: Utility, Multimedia")
+whatis("Keywords: Utility, Multimedia")
+whatis("URL: https://www.ffmpeg.org")
+whatis("Description: Cross-platform solution to record, convert and stream audio and video.")
+
+pushenv("CONDA_DEFAULT_ENV", "ffmpeg-4.3.1")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ffmpeg/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ffmpeg/envs/ffmpeg-4.3.1/bin")

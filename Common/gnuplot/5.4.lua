90e246794771f01754ea8c51374747182fc930e9
Adam Caprez <acaprez2@unl.edu>
2021-04-20 22:33:56 +0000

    Add module file for gnuplot 5.4.

--- Common/gnuplot/5.4.lua
+++ Common/gnuplot/5.4.lua
@@ -1,19 +1,19 @@
 help(
 [[
 This module loads Gnuplot.
-Version 5.2.3
+Version 5.4.1
 ]]
 )
 whatis("Name: Gnuplot")
-whatis("Version: 5.2.3")
+whatis("Version: 5.4.1")
 whatis("Category: Application")
 whatis("Keywords:  Application, Graphing")
 whatis("URL: http://www.gnuplot.info")
 whatis("Description: Gnuplot, plotting from command line")
 
-pushenv("CONDA_DEFAULT_ENV", "gnuplot-5.2.3")
+pushenv("CONDA_DEFAULT_ENV", "gnuplot-5.4.1")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gnuplot/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gnuplot/envs/gnuplot-5.2.3/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gnuplot/envs/gnuplot-5.4.1/bin")
 setenv("XDG_CACHE_HOME",pathJoin(os.getenv("WORK"),".cache"))
 
 -- the octave module includes gnuplot. trying to load them both can be problematic.

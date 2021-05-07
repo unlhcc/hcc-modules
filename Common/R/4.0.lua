0b7232feba6109d531c0a50b93736f7ce49d83d7
Adam Caprez <acaprez2@unl.edu>
2021-05-07 18:34:57 +0000

    Remove external java.
    Java is now installed in the env and R is configured to use that.

--- Common/R/4.0.lua
+++ Common/R/4.0.lua
@@ -16,14 +16,3 @@ append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.0.3/bin")
 
 family("R")
-
--- Java stuffs.  We set it here as env variables instead of in the R config to
--- make it easier to override if a different version is needed.
-setenv("JAVA_LD_LIBRARY_PATH", "/util/opt/java/1.8/jre/lib/amd64/server")
-setenv("JAVA", "/util/opt/java/1.8/jre/bin/java")
-setenv("JAVA_HOME", "/util/opt/java/1.8")
-setenv("JAVAC", "/util/opt/java/1.8/bin/javac")
-setenv("JAVAH", "/util/opt/java/1.8/bin/javah")
-setenv("JAVA_CPPFLAGS", "-I/util/opt/java/1.8/include -I/util/opt/java/1.8/include/linux")
-setenv("JAVA_LIBS", "-L/util/opt/java/jdk1.8.0_162/jre/lib/amd64/server -ljvm -Wl,-rpath=/util/opt/java/1.8/jre/lib/amd64/server")
-setenv("JAR", "/util/opt/java/1.8/bin/jar")

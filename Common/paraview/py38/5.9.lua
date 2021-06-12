b41db70d62d5d9874e646f97d354b17dbcb055ef
Adam Caprez <acaprez2@unl.edu>
2021-06-12 00:42:58 +0000

    Add module files for paraview 5.9.

---
+++ Common/paraview/py38/5.9.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Paraview.
+Version 5.9.1
+]]
+)
+whatis("Name: Paraview")
+whatis("Version: 5.9.1")
+whatis("Category: Utility, Visualization")
+whatis("Keywords: Utility, Visualization")
+whatis("URL: http://www.paraview.org")
+whatis("Description: ParaView is an open-source, multi-platform data analysis and visualization application based on Visualization Toolkit (VTK).")
+
+pushenv("CONDA_DEFAULT_ENV", "paraview-5.9.1-py38")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/paraview/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/paraview/envs/paraview-5.9.1-py38/bin")

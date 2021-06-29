bff037a21a59fd687dc964d3d7d17ef52fab44c7
Adam Caprez <acaprez2@unl.edu>
2021-06-29 23:51:28 +0000

    Add module file for oncodriveclustl 1.1.

---
+++ Common/oncodriveclustl/1.1.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads Oncodriveclustl.
+Version 1.1.3
+]]
+)
+whatis("Name: Oncodriveclustl")
+whatis("Version: 1.1.3")
+whatis("Category: Biology, Sequencing")
+whatis("Keywords: Biology, Sequencing")
+whatis("URL: https://bitbucket.org/bbglab/oncodriveclustl")
+whatis("Description: OncodriveCLUSTL is a clustering method to identify cancer drivers")
+
+pushenv("CONDA_DEFAULT_ENV", "oncodriveclustl-1.1.3")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/oncodriveclustl/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/oncodriveclustl/envs/oncodriveclustl-1.1.3/bin")
+
+local work = os.getenv("WORK") or "/tmp"
+setenv("BGDATA_LOCAL", pathJoin(work,".bgdata")

a11622ee88be67014d1c59072c24e56c9dc69f58
Adam Caprez <acaprez2@unl.edu>
2021-07-01 19:08:54 +0000

    Add python family to paraview modules.

--- Common/paraview/py37/5.9.lua
+++ Common/paraview/py37/5.9.lua
@@ -14,3 +14,5 @@ whatis("Description: ParaView is an open-source, multi-platform data analysis an
 pushenv("CONDA_DEFAULT_ENV", "paraview-5.9.1-py37")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/paraview/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/paraview/envs/paraview-5.9.1-py37/bin")
+
+family("python")

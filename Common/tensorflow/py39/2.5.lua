47c31948f351def389811c6525d2ec8f22da0b1c
Adam Caprez <acaprez2@unl.edu>
2021-07-20 21:32:02 +0000

    Add module files for tensorflow 2.5.

---
+++ Common/tensorflow/py39/2.5.lua
@@ -0,0 +1,18 @@
+help (
+[[
+This module loads tensorflow.
+Version 2.5.0
+]])
+
+whatis("Name: tensorflow")
+whatis("Version: 2.5.0")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine Learning, Mathematics")
+whatis("Description: TensorFlow is an open source software library for numerical computation using data flow graphs.")
+whatis("URL: https://www.tensorflow.org")
+
+pushenv("CONDA_DEFAULT_ENV", "tensorflow-2.5.0-py39")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow/envs/tensorflow-2.5.0-py39/bin")
+
+family("python")

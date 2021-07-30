864b787940f532703dc1dbccc2018227da433235
Adam Caprez <acaprez2@unl.edu>
2021-07-30 14:43:16 -0500

    Add module file for boxnotes2html 0.1.

---
+++ Common/boxnotes2html/0.1.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads Boxnotes2html.
+Version 0.1.4
+]]
+)
+whatis("Name: Boxnotes2html")
+whatis("Version: 0.1.4")
+whatis("Category: Utility, Editor")
+whatis("Keywords: Utility, Editor")
+whatis("URL: https://github.com/alexwennerberg/boxnotes2html")
+whatis("Description: Converting from Box Notes to HTML or other formats")
+
+pushenv("CONDA_DEFAULT_ENV", "boxnotes2html-0.1.4")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/boxnotes2html/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/boxnotes2html/envs/boxnotes2html-0.1.4/bin")
+
+family("python")

a02b4b9b19ed7c5c7054467f8363624b39009741
Adam Caprez <acaprez2@unl.edu>
2021-08-25 18:04:25 -0500

    Add module files for biopython 1.79.

---
+++ Common/biopython/py38/1.79.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Biopython.
+Version 1.79
+]]
+)
+whatis("Name: Biopython")
+whatis("Version: 1.79")
+whatis("Category: biological computation, library, development")
+whatis("Keywords: Bioinformatics, Computational Biology, Python")
+whatis("URL: http://github.com/biopython/biopython")
+whatis("Description: Collection of freely available tools for computational molecular biology")
+
+pushenv("CONDA_DEFAULT_ENV", "biopython-1.79-py38")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/biopython/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/biopython/envs/biopython-1.79-py38/bin")

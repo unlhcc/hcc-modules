66bed5cacfb6a7c9176c20252307d763b30b4a1c
Adam Caprez <acaprez2@unl.edu>
2021-09-17 16:00:09 -0500

    Add perl family to vcf-tools.

--- Common/vcftools/0.1.lua
+++ Common/vcftools/0.1.lua
@@ -15,3 +15,5 @@ whatis("Description: A set of tools written in Perl and C++ for working with VCF
 pushenv("CONDA_DEFAULT_ENV", "vcftools-0.1.16")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/vcftools/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/vcftools/envs/vcftools-0.1.16/bin")
+
+family("perl")

f89b2cde6844feefbb94ea389fc887d3b3506a6c
Adam Caprez <acaprez2@unl.edu>
2021-09-18 19:14:45 -0500

    Add module files for htseq 0.13.

---
+++ Common/htseq/py36/0.13.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads HTSeq.
+Version 0.13.5
+]]
+)
+
+whatis("Name: HTSeq")
+whatis("Version: 0.13.5")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, High-throughput Sequencing")
+whatis("URL: https://github.com/simon-anders/htseq")
+whatis("Description: HTSeq is a Python library to facilitate processing and analysis of data from high-throughput sequencing (HTS) experiments.")
+
+pushenv("CONDA_DEFAULT_ENV", "htseq-0.13.5-py36")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/htseq/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/htseq/envs/htseq-0.13.5-py36/bin")

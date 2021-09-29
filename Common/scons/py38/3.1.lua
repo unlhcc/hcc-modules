a3c04ae2a3d90d7bf548160e3b941f44ad5ae90d
Adam Caprez <acaprez2@unl.edu>
2021-09-29 13:36:33 -0500

    Change to python versioned modules.

---
+++ Common/scons/py38/3.1.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Scons.
+Version 3.1.2
+]]
+)
+whatis("Name: Scons")
+whatis("Version: 3.1.2")
+whatis("Category: utility, system")
+whatis("Keywords: Utility, System")
+whatis("Description: SCons: A software construction tool.")
+whatis("URL: http://scons.org")
+
+pushenv("CONDA_DEFAULT_ENV", "scons-3.1.2-py38")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/scons/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/scons/envs/scons-3.1.2-py38/bin")
---
+++ Common/scons/py39/4.2.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Scons.
+Version 4.2.0
+]]
+)
+whatis("Name: Scons")
+whatis("Version: 4.2.0")
+whatis("Category: utility, system")
+whatis("Keywords: Utility, System")
+whatis("Description: SCons: A software construction tool.")
+whatis("URL: http://scons.org")
+
+pushenv("CONDA_DEFAULT_ENV", "scons-4.2.0-py39")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/scons/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/scons/envs/scons-4.2.0-py39/bin")

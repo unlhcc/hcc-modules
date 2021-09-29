a853accdb4940fcfcbc3c643b059224600c6d8b0
Adam Caprez <acaprez2@unl.edu>
2021-09-28 23:45:48 -0500

    Add module file for MuJoCo 200.

---
+++ Common/mujoco/200.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads MuJoCo.
+Version 200
+]]
+)
+
+whatis("Name: MuJoCo")
+whatis("Version: 200")
+whatis("Category: Simulation, Physics")
+whatis("Keywords: Simulation, Physics")
+whatis("URL: http://www.mujoco.org/index.html")
+whatis("Description: MuJoCo | advanced physics simulation.")
+
+setenv("MJ_KEY", "/util/opt/mujoco/200/bin/mjkey.txt")
+prepend_path("PATH", "/util/opt/mujoco/200/bin")

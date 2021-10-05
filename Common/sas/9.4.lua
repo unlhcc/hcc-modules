6f9b18c06eec6d9218d0f676badaf495e5c31543
Adam Caprez <acaprez2@unl.edu>
2021-10-05 16:09:29 -0500

    Fix var handling and exit without loading on no access.

--- Common/sas/9.4.lua
+++ Common/sas/9.4.lua
@@ -1,6 +1,6 @@
 help (
 [[
-This module loads SAS. 
+This module loads SAS.
 
 Version 9.4
 ]])
@@ -9,17 +9,20 @@ whatis("Name: SAS")
 whatis("Version: 9.4")
 whatis("Category: Applications, Statistics")
 whatis("Keywords:  Mathematics, Statistics")
-whatis("Description: SAS is a software suite developed by SAS Institute for advanced analytics, business intelligence, data management, and predictive analytics." )
+whatis("Description: SAS is a software suite developed by SAS Institute for advanced analytics, business intelligence, data management, and predictive analytics.")
 whatis("URL: http://www.sas.com/en_us/software/sas9.html")
 
 if (mode() == "load") then
-    if userInGroups("sas") then
-        prepend_path("PATH"       ,"/util/opt/sas/9.4/SASFoundation/9.4")
-        prepend_path("PATH"       ,"/util/opt/sas/9.4/SASFoundation/9.4/utilities/bin")
-        --http://support.sas.com/documentation/cdl/en/hostunx/61879/HTML/default/viewer.htm#a000313346.htm
-        setenv("SASV9_OPTIONS",	"-FILELOCKS NONE")
-    else
+    if not userInGroups("sas") then
         LmodMessage("\tSAS is no longer generally available on HCC resources.")
         LmodMessage("\tContact the STATS department to purchase a license.")
+	os.exit(0)
     end
 end
+
+if userInGroups("sas") then
+  prepend_path("PATH"       ,"/util/opt/sas/9.4/SASFoundation/9.4")
+  prepend_path("PATH"       ,"/util/opt/sas/9.4/SASFoundation/9.4/utilities/bin")
+  --http://support.sas.com/documentation/cdl/en/hostunx/61879/HTML/default/viewer.htm#a000313346.htm
+  setenv("SASV9_OPTIONS", "-FILELOCKS NONE")
+ end

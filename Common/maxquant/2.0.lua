2691cfe0d4cabd70d9770d7775d5e06cf396fbb3
Adam Caprez <acaprez2@unl.edu>
2021-10-06 17:20:25 -0500

    Update maxquant to 2.0.3.0.

--- Common/maxquant/2.0.lua
+++ Common/maxquant/2.0.lua
@@ -1,24 +1,24 @@
 help(
 [[
 This module loads Maxquant.
-Version 2.0.1.0
+Version 2.0.3.0
 ]]
 )
 whatis("Name: Maxquant")
-whatis("Version: 2.0.1.0")
+whatis("Version: 2.0.3.0")
 whatis("Category: biochemistry")
 whatis("Keywords: biochemistry, proteomics, mass-spectrometric data")
 whatis("URL: http://www.coxdocs.org/doku.php?id=maxquant:start")
 whatis("Description: MaxQuant is a quantitative proteomics software package designed for analyzing large mass-spectrometric data sets. License restricted.")
 
-pushenv("CONDA_DEFAULT_ENV", "maxquant-2.0.1.0")
+pushenv("CONDA_DEFAULT_ENV", "maxquant-2.0.3.0")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.1.0/bin")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.1.0/lib/dotnet")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.3.0/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.3.0/lib/dotnet")
 
 setenv("DOTNET_ADD_GLOBAL_TOOLS_TO_PATH","false")
-setenv("DOTNET_TOOLS","/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.1.0/lib/dotnet/tools")
+setenv("DOTNET_TOOLS","/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.3.0/lib/dotnet/tools")
 setenv("DOTNET_MULTILEVEL_LOOKUP","0")
 setenv("DOTNET_SKIP_FIRST_TIME_EXPERIENCE","true")
-setenv("DOTNET_ROOT","/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.1.0/lib/dotnet")
+setenv("DOTNET_ROOT","/util/opt/anaconda/deployed-conda-envs/packages/maxquant/envs/maxquant-2.0.3.0/lib/dotnet")
 setenv("DOTNET_CLI_TELEMETRY_OPTOUT","true")

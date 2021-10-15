c8ebb23635ee39f08186e4680321b27994443b50
Adam Caprez <acaprez2@unl.edu>
2021-10-14 19:47:37 -0500

    Add module file for eigen 3.4; add EIGEN var.

--- Common/eigen/3.4.lua
+++ Common/eigen/3.4.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads Eigen.
-Version 3.3.3
+Version 3.4.0
 ]]
 )
 whatis("Name: Eigen")
-whatis("Version: 3.3.3")
+whatis("Version: 3.4.0")
 whatis("Category: C++, Linear Algebra, Library")
 whatis("Keywords: C++, Linear Algebra, Library")
 whatis("URL: http://eigen.tuxfamily.org")
 whatis("Description: C++ template library for linear algebra")
 
-pushenv("CONDA_DEFAULT_ENV", "eigen-3.3.3")
+pushenv("CONDA_DEFAULT_ENV", "eigen-3.4.0")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/eigen/envs")
-prepend_path("CPATH", "/util/opt/anaconda/deployed-conda-envs/packages/eigen/envs/eigen-3.3.3/include")
-setenv("EIGEN", "/util/opt/anaconda/deployed-conda-envs/packages/eigen/envs/eigen-3.3.3")
+prepend_path("CPATH", "/util/opt/anaconda/deployed-conda-envs/packages/eigen/envs/eigen-3.4.0/include")
+setenv("EIGEN", "/util/opt/anaconda/deployed-conda-envs/packages/eigen/envs/eigen-3.4.0")

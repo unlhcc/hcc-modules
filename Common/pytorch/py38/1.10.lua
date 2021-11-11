3c61536c0956c81ff89a2ee72a5d0b0988ceb707
Natasha Pavlovikj <npavlovikj2@unl.edu>
2021-11-11 12:52:27 -0600

    Add modules for pytorch 1.10

---
+++ Common/pytorch/py38/1.10.lua
@@ -0,0 +1,27 @@
+help(
+[[
+This module loads Pytorch.
+Version 1.10.0
+
+Note: This module includes tensorflow 2.6.0
+Note: Load the 'pytorch-gpu' module for GPU support.
+ 
+]]
+)
+whatis("Name: Pytorch")
+whatis("Version: 1.10.0")
+whatis("Category: deep learning, machine learning, mathematics")
+whatis("Keywords: deep learning, machine learning, mathematics")
+whatis("URL: https://pytorch.org/")
+whatis("Description: PyTorch is an optimized tensor library for deep learning using GPUs and CPUs.")
+
+pushenv("CONDA_DEFAULT_ENV", "pytorch-1.10.0-py38")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch/envs/pytorch-1.10.0-py38/bin")
+
+family("python")
+
+if (mode() == "load") then
+        LmodMessage("NOTE: Starting with v1.8, GPU-enabled pytorch is a separate module.")
+        LmodMessage("      Load the 'pytorch-gpu' module instead if you need GPU support.")
+end

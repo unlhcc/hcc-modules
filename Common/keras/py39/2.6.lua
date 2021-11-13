e8d57031699f53059c00998c36c26a9b230b3e79
Adam Caprez <acaprez2@unl.edu>
2021-11-12 23:13:59 -0600

    Add module file for keras 2.6.

---
+++ Common/keras/py39/2.6.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads Keras using the TensorFlow backend.
+Version 2.6.0
+
+This module includes TensorFlow 2.6.0.
+]]
+)
+
+whatis("Name: Keras")
+whatis("Version: 2.6.0")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine Learning, Mathematics")
+whatis("URL: https://keras.io")
+whatis("Description: Deep Learning Library for Theano and TensorFlow")
+
+pushenv("CONDA_DEFAULT_ENV", "keras-2.6.0-py39")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/keras/envs/keras-2.6.0-py39/bin")

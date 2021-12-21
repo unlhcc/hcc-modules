90e4cc3c9bc8d3efdfd3c55b23d7201f09cdbbf3
Adam Caprez <acaprez2@unl.edu>
2021-12-21 17:13:50 -0600

    Add module file for ucsc-bigwigaverageoverbed 377.

---
+++ Common/ucsc-bigwigaverageoverbed/377.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads ucsc-bigwigaverageoverbed.
+Version 377
+]]
+)
+whatis("Name: ucsc-bigwigaverageoverbed")
+whatis("Version: 377")
+whatis("Category: computational biology, bigwig")
+whatis("Keywords: computational biology, bigwig")
+whatis("URL: http://hgdownload.cse.ucsc.edu/admin/exe/")
+whatis("Description: Compute average score of big wig over each bed, which may have introns.")
+
+pushenv("CONDA_DEFAULT_ENV", "ucsc-bigwigaverageoverbed-377")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-bigwigaverageoverbed/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-bigwigaverageoverbed/envs/ucsc-bigwigaverageoverbed-377/bin")

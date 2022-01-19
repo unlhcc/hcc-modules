086130467efd94fd5b88b2844b87c7b289c1871a
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-01-19 09:52:48 -0600

    Update rmats module

--- Common/rmats/4.1.lua
+++ Common/rmats/4.1.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads rMATS.
-Version 4.1.0
+Version 4.1.2
 ]]
 )
 
 whatis("Name: rMATS")
-whatis("Version: 4.1.0")
+whatis("Version: 4.1.2")
 whatis("Category: computational biology, alternative splicing")
 whatis("Keywords: computational biology, RNA-Seq, alternative splicing")
 whatis("URL: http://rnaseq-mats.sourceforge.net")
 whatis("Description: MATS is a computational tool to detect differential alternative splicing events from RNA-Seq data.")
 
-pushenv("CONDA_DEFAULT_ENV", "rmats-4.1.0")
+pushenv("CONDA_DEFAULT_ENV", "rmats-4.1.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rmats/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rmats/envs/rmats-4.1.0/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rmats/envs/rmats-4.1.2/bin")

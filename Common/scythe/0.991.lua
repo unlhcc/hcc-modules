ec29df96a07879074fff5477f5d35f6d9f99a733
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-02-03 11:42:16 -0600

    Update scythe module

--- Common/scythe/0.991.lua
+++ Common/scythe/0.991.lua
@@ -15,3 +15,5 @@ whatis("Description: A 3'-end adapter contaminant trimmer.")
 pushenv("CONDA_DEFAULT_ENV", "scythe-0.991")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/scythe/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/scythe/envs/scythe-0.991/bin")
+
+setenv("SCYTHE_ADAPTERS", "/util/opt/anaconda/deployed-conda-envs/packages/scythe/envs/scythe-0.991/share/scythe-0.991-1/")

596bf82f94dc629b71e784ada009fd0de6252e3f
Natasha Pavlovikj <natasha.pavlovikj@huskers.unl.edu>
2022-02-03 20:01:49 -0600

    Update prokka module file

--- Common/prokka/1.14.lua
+++ Common/prokka/1.14.lua
@@ -1,16 +1,16 @@
 help (
 [[
 This module loads Prokka.
-Version 1.14.5
+Version 1.14.6
 ]])
 
 whatis("Name: Prokka")
-whatis("Version: 1.14.5")
+whatis("Version: 1.14.6")
 whatis("Category: bioinformatics, annotation, prokaryotic")
 whatis("Keywords:  Bioinformatics, Annotation")
 whatis("Description: Prokka is a software tool for the rapid annotation of prokaryotic genomes.")
 whatis("URL: http://www.vicbioinformatics.com/software.prokka.shtml")
 
-pushenv("CONDA_DEFAULT_ENV", "prokka-1.14.5")
-prepend_path{"PATH", "/util/opt/anaconda/deployed-conda-envs/packages/prokka/envs/prokka-1.14.5/bin", priority=100}
+pushenv("CONDA_DEFAULT_ENV", "prokka-1.14.6")
+prepend_path{"PATH", "/util/opt/anaconda/deployed-conda-envs/packages/prokka/envs/prokka-1.14.6/bin", priority=100}
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/prokka/envs")

c23513e256da648bdf561e3f7be7b35b02574641
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-02-09 00:22:15 -0600

    Update FastTree module

--- Common/fasttree/2.1.lua
+++ Common/fasttree/2.1.lua
@@ -2,16 +2,16 @@ help (
 [[
 This module loads FastTree
 PATH has been prepended to include the FastTree program.
-Version 2.1.10
+Version 2.1.11
 ]])
 
 whatis("Name: FastTree")
-whatis("Version: 2.1.10")
+whatis("Version: 2.1.11")
 whatis("Category: computational biology, phylogenetics")
 whatis("Keywords: Biology, Genomics, Phylogenetics")
 whatis("Description: FastTree infers approximately-maximum-likelihood phylogenetic trees from alignments of nucleotide or protein sequences.")
 whatis("URL: http://www.microbesonline.org/fasttree")
 
-prepend_path{"PATH"		,"/util/opt/anaconda/deployed-conda-envs/packages/fasttree/envs/fasttree-2.1.10/bin",priority=100}
-pushenv("CONDA_DEFAULT_ENV"	,"fasttree-2.1.10")
+prepend_path{"PATH"		,"/util/opt/anaconda/deployed-conda-envs/packages/fasttree/envs/fasttree-2.1.11/bin",priority=100}
+pushenv("CONDA_DEFAULT_ENV"	,"fasttree-2.1.11")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/fasttree/envs")

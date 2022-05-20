ef6fc19bd8e8e26d57649f7611b459385abe925c
Adam Caprez <acaprez2@unl.edu>
2022-05-19 22:21:53 -0500

    Add module file for masurca 4.0.

---
+++ Common/masurca/4.0.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Masurca.
+Version 4.0.9
+]]
+)
+whatis("Name: Masurca")
+whatis("Version: 4.0.9")
+whatis("Category: Biology, Genomics, Assembly")
+whatis("Keywords: Biology, Genomics, Assembly")
+whatis("URL: http://masurca.blogspot.co.uk/")
+whatis("Description: MaSuRCA (Maryland Super-Read Celera Assembler) genome assembly software. MaSuRCA requires Illumina data, and supports third-generation PacBio/Nanopore MinION reads for hybrid assembly.")
+
+pushenv("CONDA_DEFAULT_ENV", "masurca-4.0.9")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/masurca/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/masurca/envs/masurca-4.0.9/bin")

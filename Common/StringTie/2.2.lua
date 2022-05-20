ddf92bb47fea1f921673b1c7561e09147c30cbf4
Adam Caprez <acaprez2@unl.edu>
2022-05-19 22:21:02 -0500

    Add module file for StringTie 2.2.

---
+++ Common/StringTie/2.2.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads StringTie.
+Version 2.2.1
+]]
+)
+
+whatis("Name: StringTie")
+whatis("Version: 2.2.1")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment, Sequencing")
+whatis("URL: https://ccb.jhu.edu/software/stringtie/")
+whatis("Description: StringTie is a fast and highly efficient assembler of RNA-Seq alignments into potential transcripts.")
+
+pushenv("CONDA_DEFAULT_ENV", "stringtie-2.2.1")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/stringtie/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/stringtie/envs/stringtie-2.2.1/bin")

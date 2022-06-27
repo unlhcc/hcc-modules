56591342485c63fc16ee97ecc96e88bde78aa9a3
Adam Caprez <acaprez2@unl.edu>
2022-06-27 18:18:42 -0500

    Fix line breaks.

--- Common/uropa/4.0.lua
+++ Common/uropa/4.0.lua
@@ -9,10 +9,7 @@ whatis("Version: 4.0.2")
 whatis("Category: Biology, Genomics")
 whatis("Keywords: Biology, Genomics")
 whatis("URL: https://github.molgen.mpg.de/loosolab/UROPA")
-whatis("Description: UROPA (Universal RObust Peak Annotator) is a command line based tool, intended for genomic region annotation from e.g. peak calling.
-It detects the most appropriate annotation by taking parameters such as feature type, anchor, direction and strand into account.
-Furthermore, it allows filtering for GTF attribute values, e.g. protein_coding.
-")
+whatis("Description: UROPA (Universal RObust Peak Annotator) is a command line based tool, intended for genomic region annotation from e.g. peak calling. It detects the most appropriate annotation by taking parameters such as feature type, anchor, direction and strand into account. Furthermore, it allows filtering for GTF attribute values, e.g. protein_coding.")
 
 pushenv("CONDA_DEFAULT_ENV", "uropa-4.0.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/uropa/envs")

cba42d3d9831e87175abd3adfd114d1d11d66033
Adam Caprez <acaprez2@unl.edu>
2022-07-21 19:45:54 -0500

    Add module file for ilastik 1.3.

---
+++ Common/ilastik/1.3.lua
@@ -0,0 +1,14 @@
+help(
+[[
+This module loads ilastik.
+Version 1.3.3post3
+]]
+)
+whatis("Name: ilastik")
+whatis("Version: 1.3.3post3")
+whatis("Category: Image Processing, Image Analysis")
+whatis("Keywords: Image Processing, Image Analysis")
+whatis("URL: https://www.ilastik.org/index.html")
+whatis("Description: ilastik is a gui driven open source interactive image classification, segmentation and analysis tool that uses machine learning.")
+
+prepend_path("PATH", "/util/opt/ilastik/1.3")

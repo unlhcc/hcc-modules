51ab124390b1d48f6e0e7c3155060fe0bd881a8b
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-09-01 22:20:16 -0500

    Update blast version

---
+++ Common/blast/2.13.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads blast.
+Version 2.13.0
+]]
+)
+
+whatis("Name: NCBI BLAST+")
+whatis("Version: 2.13.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment")
+whatis("URL: http://www.ncbi.nlm.nih.gov/BLAST")
+whatis("Description: NCBI BLAST+ sequence alignment package. The program compares nucleotide or protein sequences to sequence databases and calculates the statistical significance of matches.")
+
+pushenv("CONDA_DEFAULT_ENV", "blast-2.13.0")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/blast/envs/blast-2.13.0/bin")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/blast/envs")

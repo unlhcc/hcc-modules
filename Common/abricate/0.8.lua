19ea8b1c2c1781fe930e0f48f63f863977d4f121
Adam Caprez <acaprez2@unl.edu>
2022-10-06 14:30:43 -0500

    Add perl family to abricate modules.

--- Common/abricate/0.8.lua
+++ Common/abricate/0.8.lua
@@ -14,3 +14,5 @@ whatis("Description: Mass screening of contigs for antibiotic resistance genes")
 pushenv("CONDA_DEFAULT_ENV", "abricate-0.8.13")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/abricate/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/abricate/envs/abricate-0.8.13/bin")
+
+family("perl")
--- Common/abricate/1.0.lua
+++ Common/abricate/1.0.lua
@@ -14,3 +14,5 @@ whatis("Description: Mass screening of contigs for antibiotic resistance genes")
 pushenv("CONDA_DEFAULT_ENV", "abricate-1.0.1")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/abricate/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/abricate/envs/abricate-1.0.1/bin")
+
+family("perl")

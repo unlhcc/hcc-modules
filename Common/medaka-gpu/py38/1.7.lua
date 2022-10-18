ee7b9db83a08123e2d2c3431269d18345fee4b0f
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-10-18 12:27:56 -0500

    Add python family

--- Common/medaka-gpu/py38/1.7.lua
+++ Common/medaka-gpu/py38/1.7.lua
@@ -15,3 +15,5 @@ whatis("Description: Neural network sequence error correction.")
 pushenv("CONDA_DEFAULT_ENV", "medaka-gpu-1.7.2-py38")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/medaka-gpu/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/medaka-gpu/envs/medaka-gpu-1.7.2-py38/bin")
+
+family("python")

73ab68d272e15b402cf248235bec7a982d11c0aa
Natasha Pavlovikj <npavlovikj2@unl.edu>
2022-10-20 07:19:22 -0500

    Update busco env

--- Common/busco/py39/5.4.lua
+++ Common/busco/py39/5.4.lua
@@ -1,7 +1,7 @@
 help(
 [[
 This module loads BUSCO.
-Version 5.4.2
+Version 5.4.3
 ]]
 )
 
@@ -12,16 +12,16 @@ if mode() == "load" then
 end
 
 whatis("Name: BUSCO")
-whatis("Version: 5.4.2")
+whatis("Version: 5.4.3")
 whatis("Category: computational biology, benchmarking")
 whatis("Keywords: genome assembly, assessment, annotation")
 whatis("URL: https://busco.ezlab.org/")
 whatis("Description: BUSCO provides measures for quantitative assessment of genome assembly, gene set, and transcriptome completeness based on evolutionarily informed expectations of gene content from near-universal single-copy orthologs selected from OrthoDB.")
 
 load("biodata/1.0")
-pushenv("CONDA_DEFAULT_ENV", "busco-5.4.2-py39")
+pushenv("CONDA_DEFAULT_ENV", "busco-5.4.3-py39")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-5.4.2-py39/bin")
-prepend_path("BUSCO_CONFIG_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-5.4.2-py39/config/config.ini")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-5.4.3-py39/bin")
+prepend_path("BUSCO_CONFIG_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/busco/envs/busco-5.4.3-py39/config/config.ini")
 
 family("python")

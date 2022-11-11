c1e25b5ce75f213ecabc0d9249b55275e5cc8b45
Adam Caprez <acaprez2@unl.edu>
2022-11-11 14:23:50 -0600

    Add go.unl link.

--- Common/singularity/3.8.lua
+++ Common/singularity/3.8.lua
@@ -14,7 +14,7 @@ whatis("Description: Application containers enabling mobility of compute.")
 whatis("URL: https://sylabs.io/docs")
 
 if mode() == "load" then
-    LmodMessage("\tMessage from the singularity module: \n\tSingularity is now Apptainer. Please update your SLURM scripts to load the apptainer module and use the 'apptainer' command instead. \n\tFor compatibility, this module temporarily redirects there.")
+    LmodMessage("\tMessage from the singularity module: \n\tSingularity is now Apptainer. Please update your SLURM scripts to load the apptainer module and use the 'apptainer' command instead. \n\tFor compatibility, this module temporarily redirects there. For more information, see https://go.unl.edu/xeu7.")
 end
 
 load("apptainer/1.1")

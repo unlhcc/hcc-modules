aa1f3eeb39d7937682d540b562d26f06869053fa
Emelie Harstad <eharstad@unl.edu>
2022-11-30 20:40:20 +0000

    updated flye module to 2.9.1

--- Common/flye/2.9.lua
+++ Common/flye/2.9.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads Flye.
-Version 2.9
+Version 2.9.1
 ]]
 )
 
 whatis("Name: Flye")
-whatis("Version: 2.9")
+whatis("Version: 2.9.1")
 whatis("Category: Bioinformatics, Sequencing")
 whatis("Keywords: Bioinformatics, Sequencing")
 whatis("URL: https://github.com/fenderglass/Flye/")
 whatis("Description: Fast and accurate de novo assembler for single molecule sequencing reads")
 
-pushenv("CONDA_DEFAULT_ENV", "flye-2.9")
+pushenv("CONDA_DEFAULT_ENV", "flye-2.9.1")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/flye/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/flye/envs/flye-2.9/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/flye/envs/flye-2.9.1/bin")

ab2245eafe8333a8766d568a9309bca0e9fa8998
Adam Caprez <acaprez2@unl.edu>
2023-02-17 17:06:20 -0600

    Update star to 2.7.10b.

--- Common/star/2.7.lua
+++ Common/star/2.7.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads Star.
-Version 2.7.6a
+Version 2.7.10b
 ]]
 )
 
 whatis("Name: Star")
-whatis("Version: 2.7.6a")
+whatis("Version: 2.7.10b")
 whatis("Category: biology, genomics")
 whatis("Keywords: Biology, Genomics")
 whatis("URL: https://github.com/alexdobin/STAR")
 whatis("Description: An RNA-seq read aligner.")
 
-pushenv("CONDA_DEFAULT_ENV", "star-2.7.6a")
+pushenv("CONDA_DEFAULT_ENV", "star-2.7.10b")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/star/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/star/envs/star-2.7.6a/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/star/envs/star-2.7.10b/bin")

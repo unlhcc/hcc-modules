8ed87c691f78d8a8653973d91d6a53546d9e6685
Adam Caprez <acaprez2@unl.edu>
2023-02-17 18:19:34 -0600

    Add module for star 2.7.9a.

---
+++ Common/star/2.7.9a.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Star.
+Version 2.7.9a
+]]
+)
+
+whatis("Name: Star")
+whatis("Version: 2.7.9a")
+whatis("Category: biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: https://github.com/alexdobin/STAR")
+whatis("Description: An RNA-seq read aligner.")
+
+pushenv("CONDA_DEFAULT_ENV", "star-2.7.9a")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/star/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/star/envs/star-2.7.9a/bin")

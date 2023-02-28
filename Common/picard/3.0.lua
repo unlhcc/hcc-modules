f8630197c2c74560002efe39dd6f7d8b1f55bdf6
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-02-28 12:52:09 -0600

    Add module file for picard

---
+++ Common/picard/3.0.lua
@@ -0,0 +1,26 @@
+help (
+[[
+This module loads Picard.
+Version 3.0.0
+]])
+
+if mode() == "load" then
+  LmodMessage("-------------------------| Help message for picard module |-------------------------------")
+  LmodMessage("Usage: picard <program name> <arguments>")
+  LmodMessage("You can also pass -Xms or -Xmx arguments to control Java memory allocation.")
+  LmodMessage("For example, to increase Java heap space to 10GB, run: picard -Xms512m -Xmx10g <program name> <arguments>")
+  LmodMessage("------------------------------------------------------------------------------------------")
+end
+
+whatis("Name: Picard")
+whatis("Version: 3.0.0")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment, Sequencing, File processing")
+whatis("URL: http://broadinstitute.github.io/picard/")
+whatis("Description: A set of Java command line tools for manipulating high-throughput sequencing data (HTS) data and formats. ")
+
+prepend_path{"PATH", "/util/opt/anaconda/deployed-conda-envs/packages/picard/envs/picard-3.0.0/bin", priority=100}
+pushenv("CONDA_DEFAULT_ENV", "picard-3.0.0")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/picard/envs")
+
+family("R")

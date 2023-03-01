5415fa577155d16bf6d672ca28f80b2b5d7bc922
Adam Caprez <acaprez2@unl.edu>
2023-03-01 14:51:25 -0600

    Add module files for java 17 and 19.

---
+++ Common/java/17.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads Java.
+
+Version 17
+]])
+
+whatis("Name: java")
+whatis("Version: 17")
+whatis("Category: system, compiler")
+whatis("Keywords:  system, utility, compiler")
+whatis("Description: Java JDK.")
+whatis("URL: http://www.java.com")
+
+prepend_path("PATH"       ,"/util/opt/java/17/bin")
+setenv("JAVA_HOME"	  ,"/util/opt/java/17")
---
+++ Common/java/19.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads Java.
+
+Version 19
+]])
+
+whatis("Name: java")
+whatis("Version: 19")
+whatis("Category: system, compiler")
+whatis("Keywords:  system, utility, compiler")
+whatis("Description: Java JDK.")
+whatis("URL: http://www.java.com")
+
+prepend_path("PATH"       ,"/util/opt/java/19/bin")
+setenv("JAVA_HOME"	  ,"/util/opt/java/19")

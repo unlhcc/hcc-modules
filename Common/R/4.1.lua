78cd526dfc504aed076476303923d007de9c2a53
Adam Caprez <acaprez2@unl.edu>
2023-03-08 13:22:50 -0600

    Update R 4.1 and 4.2 envs.

--- Common/R/4.1.lua
+++ Common/R/4.1.lua
@@ -5,14 +5,14 @@ Version 4.1
 ]]
 )
 whatis("Name: R")
-whatis("Version: 4.1")
+whatis("Version: 4.1.3")
 whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: http://www.r-project.org/")
 whatis("Description: R is a free software environment for statistical computing and graphics.")
 
-pushenv("CONDA_DEFAULT_ENV", "r-4.1.1")
+pushenv("CONDA_DEFAULT_ENV", "r-4.1.3")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.1.1/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.1.3/bin")
 
 family("R")
--- Common/R/4.2.lua
+++ Common/R/4.2.lua
@@ -5,14 +5,14 @@ Version 4.2
 ]]
 )
 whatis("Name: R")
-whatis("Version: 4.2")
+whatis("Version: 4.2.2")
 whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: http://www.r-project.org/")
 whatis("Description: R is a free software environment for statistical computing and graphics.")
 
-pushenv("CONDA_DEFAULT_ENV", "r-4.2.0")
+pushenv("CONDA_DEFAULT_ENV", "r-4.2.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.2.0/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.2.2/bin")
 
 family("R")

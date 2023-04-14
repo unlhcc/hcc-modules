8c9f1b9710c2d4da36c8cb52dcab4f0e18b073ac
Adam Caprez <acaprez2@unl.edu>
2023-04-14 16:18:28 -0500

    Add module files for TF-COMB 1.0.

---
+++ Common/tf-comb/py38/1.0.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads TF-COMB.
+Version 1.0.3
+]]
+)
+whatis("Name: TF-COMB")
+whatis("Version: 1.0.3")
+whatis("Category: Bioinformatics")
+whatis("Keywords: Bioinformatics")
+whatis("URL: https://tf-comb.readthedocs.io/")
+whatis("Description: Transcription Factor Co-Occurrence using Market Basket analysis")
+
+pushenv("CONDA_DEFAULT_ENV", "tf-comb-1.0.3-py38")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tf-comb/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tf-comb/envs/tf-comb-1.0.3-py38/bin")
+
+family("python")
+family("R")

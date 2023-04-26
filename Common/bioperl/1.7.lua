d6d0fe94cd8c4cd073e80be40706ee35a6e7dced
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-04-26 11:42:34 -0500

    Add family perl to bioperl

--- Common/bioperl/1.7.lua
+++ Common/bioperl/1.7.lua
@@ -15,3 +15,5 @@ whatis("Description: BioPerl is a collection of Perl modules that facilitate the
 pushenv("CONDA_DEFAULT_ENV", "bioperl-1.7.8")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/bioperl/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/bioperl/envs/bioperl-1.7.8/bin")
+
+family("perl")

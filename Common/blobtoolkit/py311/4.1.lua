90d558bd9f7d2a6d1e8ddc99e39bbd80982652c2
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-05-01 12:47:49 -0500

    Add module file for blobtoolkit

---
+++ Common/blobtoolkit/py311/4.1.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads BlobToolKit.
+Version 4.1.4
+]]
+)
+
+whatis("Name: BlobToolKit")
+whatis("Version: 4.1.4")
+whatis("Category: bioinformatics, genomics")
+whatis("Keywords: genomics, QC, visualisation")
+whatis("URL: https://github.com/blobtoolkit/blobtoolkit")
+whatis("Description: Interactive quality assessment of genome assemblies.")
+
+pushenv("CONDA_DEFAULT_ENV", "blobtoolkit-4.1.4-py311")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/blobtoolkit/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/blobtoolkit/envs/blobtoolkit-4.1.4-py311/bin")
+
+family("python")

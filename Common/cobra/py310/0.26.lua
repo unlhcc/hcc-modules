d77737130d7709de9aae1c0a4a753b71903b595d
Adam Caprez <acaprez2@unl.edu>
2023-05-08 20:13:01 -0500

    Add module files for cobra 0.26.

---
+++ Common/cobra/py310/0.26.lua
@@ -0,0 +1,32 @@
+help(
+[[
+This module loads Cobra.
+Version 0.26.3
+]]
+)
+whatis("Name: Cobra")
+whatis("Version: 0.26.3")
+whatis("Category: Biology, Modeling")
+whatis("Keywords: Biology, Modeling")
+whatis("URL: https://opencobra.github.io/cobrapy")
+whatis("Description: COBRApy is a package for constraints-based modeling of metabolic networks.")
+
+pushenv("CONDA_DEFAULT_ENV", "cobra-0.26.3-py310")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cobra/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cobra/envs/cobra-0.26.3-py310/bin")
+
+-- gurobi
+setenv("GUROBI_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/cobra/envs/cobra-0.26.3-py310")
+setenv("GUROBI_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cobra/envs/cobra-0.26.3-py310")
+setenv("GRB_LICENSE_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/cobra/gurobi.lic")
+setenv("LAZYLPSOLVERLIBS_GUROBI_LIB", "/util/opt/anaconda/deployed-conda-envs/packages/cobra/envs/cobra-0.26.3-py310/lib/libgurobi100.so")
+
+-- cplex
+prepend_path("PATH",     "/util/opt/IBM-ILOG-CPLEX/22.1.0/cplex/bin/x86-64_linux")
+prepend_path("PATH",     "/util/opt/IBM-ILOG-CPLEX/22.1.0/cpoptimizer/bin/x86-64_linux")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/IBM-ILOG-CPLEX/22.1.0/cplex/bin/x86-64_linux")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/IBM-ILOG-CPLEX/22.1.0/cpoptimizer/bin/x86-64_linux")
+prepend_path("PATH",     "/util/opt/IBM-ILOG-CPLEX/22.1.0/opl/oplide")
+prepend_path("ILOG_CPLEX_PATH",     "/util/opt/IBM-ILOG-CPLEX/22.1.0")
+
+family("python")

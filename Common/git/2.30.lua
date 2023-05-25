f6b4ead71c8e88779caf68a26a3c6e11cec70d05
Adam Caprez <acaprez2@unl.edu>
2023-05-25 14:03:32 -0500

    Update git versions.

--- Common/git/2.30.lua
+++ Common/git/2.30.lua
@@ -1,16 +1,16 @@
 help(
 [[
 This module loads Git and git-lfs.
-Version 2.30.2
+Version 2.30.9
 ]]
 )
 whatis("Name: Git")
-whatis("Version: 2.30.2")
+whatis("Version: 2.30.9")
 whatis("Category: library, tools")
 whatis("Keywords: System, Source Control Management, Tools")
 whatis("URL: http://git-scm.com")
 whatis("Description: distributed version control system")
 
-pushenv("CONDA_DEFAULT_ENV", "git-2.30.2")
+pushenv("CONDA_DEFAULT_ENV", "git-2.30.9")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/git/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/git/envs/git-2.30.2/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/git/envs/git-2.30.9/bin")
--- Common/git/2.40.lua
+++ Common/git/2.40.lua
@@ -1,16 +1,16 @@
 help(
 [[
 This module loads Git and git-lfs.
-Version 2.39.3
+Version 2.40.1
 ]]
 )
 whatis("Name: Git")
-whatis("Version: 2.39.3")
+whatis("Version: 2.40.1")
 whatis("Category: library, tools")
 whatis("Keywords: System, Source Control Management, Tools")
 whatis("URL: http://git-scm.com")
 whatis("Description: distributed version control system")
 
-pushenv("CONDA_DEFAULT_ENV", "git-2.39.3")
+pushenv("CONDA_DEFAULT_ENV", "git-2.40.1")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/git/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/git/envs/git-2.39.3/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/git/envs/git-2.40.1/bin")

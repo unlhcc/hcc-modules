addf26b70907ecdc5d2164c454b76183ae9fe05f
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-06-12 00:06:12 -0500

    Add TCIA dataset variable

--- Common/mldata/1.0.lua
+++ Common/mldata/1.0.lua
@@ -11,6 +11,12 @@ $TCGA                   TCGA database (most recent version)
 $TCGA_MAY2022           TCGA database retrieved May 2022 (current same as most recent)
 $CAMELYON16             CAMELYON 16 database
 $CAMELYON17             CAMELYON 17 database
+$TCIA                   TCIA database (most recent version)
+$TCIA_JUNE2023          TCIA database retrieved June 2023 (current same as most recent)
+
+You can alsways check what's available with, e.g.,
+
+ls $TCIA
 
 ]])
 
@@ -30,6 +36,9 @@ local tcga_may2022 = pathJoin(tcga, "may2022")
 local camelyon = pathJoin(data_root,"camelyon")
 local camelyon16 = pathJoin(camelyon, "16")
 local camelyon17 = pathJoin(camelyon, "17")
+local tcia = pathJoin(data_root, "tcia")
+local tcia_latest = pathJoin(tcia, "latest")
+local tcia_june2023 = pathJoin(tcia, "june2023")
 
 setenv("MLDATA", data_root)
 setenv("IMAGENET", imagenet_latest)
@@ -38,6 +47,8 @@ setenv("TCGA", tcga_latest)
 setenv("TCGA_MAY2022", tcga_may2022)
 setenv("CAMELYON16", camelyon16)
 setenv("CAMELYON17", camelyon17)
+setenv("TCIA", tcia_latest)
+setenv("TCIA_JUNE2023", tcia_june2023)
 
 if (mode() == "load") then
         LmodMessage("Static data resources for machine learning/AI 1.0.")

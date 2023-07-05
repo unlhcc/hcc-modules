b264eba3e8014caa9d6a1aeb34448ba937f7fa61
Adam Caprez <acaprez2@unl.edu>
2023-07-05 18:34:06 -0500

    Update gurobi to 10.0.2 and include matlab.

--- Common/gurobi/py39/10.0.lua
+++ Common/gurobi/py39/10.0.lua
@@ -1,23 +1,23 @@
 help(
 [[
 This module loads Gurobi.
-Version 10.0.0
+Version 10.0.2
 ]]
 )
 whatis("Name: Gurobi")
-whatis("Version: 10.0.0")
+whatis("Version: 10.0.2")
 whatis("Category: Mathematics, Optimization")
 whatis("Keywords: Mathematics, Optimization")
 whatis("URL: http://www.gurobi.com")
 whatis("Description: The Gurobi Optimizer is the state-of-the-art math programming solver able to handle all major problem types.")
 
-pushenv("CONDA_DEFAULT_ENV", "gurobi-10.0.0-py39")
+pushenv("CONDA_DEFAULT_ENV", "gurobi-10.0.2-py39")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.0-py39/bin")
-setenv("GUROBI_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.0-py39")
-setenv("GUROBI_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.0-py39")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py39/bin")
+setenv("GUROBI_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py39")
+setenv("GUROBI_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py39")
 setenv("GRB_LICENSE_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/gurobi.lic")
---prepend_path("MATLABPATH","/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.0-py39/share/gurobi-10.0.0-0/matlab")
-setenv("LAZYLPSOLVERLIBS_GUROBI_LIB", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.0-py39/lib/libgurobi100.so")
+prepend_path("MATLABPATH","/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py39/share/gurobi-matlab-10.0.2-0/matlab")
+setenv("LAZYLPSOLVERLIBS_GUROBI_LIB", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py39/lib/libgurobi100.so")
 
 family("python")

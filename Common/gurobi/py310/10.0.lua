7ff83e2151ccbb9cc0ad1b5e826f929b14aff751
Adam Caprez <acaprez2@unl.edu>
2023-07-05 20:18:46 -0500

    Add library path for gurobi.

--- Common/gurobi/py310/10.0.lua
+++ Common/gurobi/py310/10.0.lua
@@ -14,6 +14,7 @@ whatis("Description: The Gurobi Optimizer is the state-of-the-art math programmi
 pushenv("CONDA_DEFAULT_ENV", "gurobi-10.0.2-py310")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py310/bin")
+prepend_path("LD_LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py310/lib")
 setenv("GUROBI_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py310")
 setenv("GUROBI_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-10.0.2-py310")
 setenv("GRB_LICENSE_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/gurobi.lic")

4ae571468168f1f6e03fa640f263086b5d09da74
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-07-20 16:08:59 -0500

    Update igvtools modules

--- Common/igvtools/2.14.lua
+++ Common/igvtools/2.14.lua
@@ -1,19 +1,19 @@
 help(
 [[
 This module loads IGVtools.
-Version 2.5.3
+Version 2.14.1
 ]]
 )
 
 whatis("Name: IGVtools")
-whatis("Version: 2.5.3")
+whatis("Version: 2.14.1")
 whatis("Category: genomics,visualization")
 whatis("Keywords: genomics,metadata,viewer")
 whatis("URL: https://software.broadinstitute.org/software/igv/")
 whatis("Description: Command line tools for IGV")
 
-pushenv("CONDA_DEFAULT_ENV", "igvtools-2.5.3")
+pushenv("CONDA_DEFAULT_ENV", "igvtools-2.14.1")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/igvtools/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/igvtools/envs/igvtools-2.5.3/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/igvtools/envs/igvtools-2.14.1/bin")
 
 family("java")

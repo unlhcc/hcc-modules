b3ce5620c1cc7210f9b5bbb7d037719312647c33
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-07-21 15:52:08 -0500

    Add module file for julia 1.9 and update JULIA_DEPOT_PATH var

--- Common/julia/1.9.lua
+++ Common/julia/1.9.lua
@@ -1,22 +1,22 @@
 help(
 [[
 This module loads Julia.
-Version 1.7.2
+Version 1.9.2
 ]]
 )
 whatis("Name: Julia")
-whatis("Version: 1.7.2")
+whatis("Version: 1.9.2")
 whatis("Category: Language, System, Julia")
 whatis("Keywords: Language, System, Julia")
 whatis("URL: http://julialang.org/")
 whatis("Description: A high-performance dynamic programming language for data science")
 
-pushenv("CONDA_DEFAULT_ENV", "julia-1.7.2")
+pushenv("CONDA_DEFAULT_ENV", "julia-1.9.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/julia/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/julia/envs/julia-1.7.2/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/julia/envs/julia-1.9.2/bin")
 
 local home = os.getenv("HOME") or "/tmp"
-setenv("JULIA_DEPOT_PATH", ":/util/opt/anaconda/deployed-conda-envs/packages/julia/envs/julia-1.7.2/share/julia")
+setenv("JULIA_DEPOT_PATH", ":/util/opt/anaconda/deployed-conda-envs/packages/julia/envs/julia-1.9.2/share/julia")
 append_path("JULIA_DEPOT_PATH", pathJoin(home,".julia"))
 setenv("JULIA_PROJECT", pathJoin(home,".julia"))
-setenv("JULIA_LOAD_PATH","@:@julia-1.7.2:@stdlib")
+setenv("JULIA_LOAD_PATH","@:@julia-1.9.2:@stdlib")

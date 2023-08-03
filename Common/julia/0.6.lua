e02f0d54a08036c8aa3c4d247ebef6d008b0ebb3
Adam Caprez <acaprez2@unl.edu>
2023-08-03 14:57:13 -0500

    Move old julia module from Core/.

---
+++ Common/julia/0.6.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads julia.
+
+Version 0.6.2
+]])
+
+whatis("Name: julia")
+whatis("Version: 0.6.2")
+whatis("Category: Language, System, Julia")
+whatis("Keywords:  Language, System, Julia")
+whatis("Description: Julia is a high-level, high-performance dynamic programming language for numerical computing.")
+whatis("URL: https://julialang.org")
+
+prepend_path("PATH"       ,"/util/opt/julia/0.6/bin")
+prepend_path("MANPATH"	  ,"/util/opt/julia/0.6/share/man")

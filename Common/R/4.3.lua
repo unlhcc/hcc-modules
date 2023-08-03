0b5dabeccacdced7db6f2b6af72b416eb697ef50
Adam Caprez <acaprez2@unl.edu>
2023-08-03 17:25:10 -0500

    Add module file for R 4.3.

---
+++ Common/R/4.3.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads R.
+Version 4.3
+]]
+)
+whatis("Name: R")
+whatis("Version: 4.3.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.r-project.org/")
+whatis("Description: R is a free software environment for statistical computing and graphics.")
+
+pushenv("CONDA_DEFAULT_ENV", "r-4.3.1")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r/envs/r-4.3.1/bin")
+
+family("R")

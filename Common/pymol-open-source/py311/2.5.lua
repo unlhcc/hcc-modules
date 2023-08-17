075760b6899c9d3c854ca85f233eebcbf5ea822e
Adam Caprez <acaprez2@unl.edu>
2023-08-17 15:43:14 -0500

    Add modules for pymol-open-source 2.5.

--- Common/pymol-open-source/py311/2.5.lua
+++ Common/pymol-open-source/py311/2.5.lua
@@ -1,18 +1,18 @@
 help(
 [[
 This module loads Pymol-open-source.
-Version 2.4.0
+Version 2.5.0
 ]]
 )
 whatis("Name: Pymol-open-source")
-whatis("Version: 2.4.0")
+whatis("Version: 2.5.0")
 whatis("Category: Molecular Dynamics, Visualization")
 whatis("Keywords: Molecular Dynamics, Visualization")
 whatis("URL: https://pymol.org/")
 whatis("Description: A user-sponsored molecular visualization system")
 
-pushenv("CONDA_DEFAULT_ENV", "pymol-open-source-2.4.0-py38")
+pushenv("CONDA_DEFAULT_ENV", "pymol-open-source-2.5.0-py311")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pymol-open-source/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pymol-open-source/envs/pymol-open-source-2.4.0-py38/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pymol-open-source/envs/pymol-open-source-2.5.0-py311/bin")
 
 family("python")

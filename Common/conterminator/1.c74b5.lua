1603078eb8cd9c0bb42d90d5be148c739288ddb2
Cloud User <almalinux@alma-linux-machine.novalocal>
2023-08-18 19:38:45 +0000

    conterminator

---
+++ Common/conterminator/1.c74b5.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads conterminator.
+Version 1.c74b5
+]]
+)
+
+whatis("Name: Conterminator")
+whatis("Version: 1.c74b5")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics")
+whatis("URL: https://github.com/martin-steinegger/conterminator/")
+whatis("Description: Efficient method for detecting incorrectly labeled sequences across kingdoms")
+
+pushenv("CONDA_DEFAULT_ENV", "conterminator-1.c74b5")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/conterminator/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/conterminator/envs/conterminator-1.c74b5/bin")

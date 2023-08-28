bbe6b7b4bb6a59cbf12ab3ebef6c0b0300d28409
Adam Caprez <acaprez2@unl.edu>
2023-08-28 17:44:25 -0500

    Add module file for gstreamer 1.22.

---
+++ Common/gstreamer/1.22.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Gstreamer.
+Version 1.22.5
+]]
+)
+whatis("Name: Gstreamer")
+whatis("Version: 1.22.5")
+whatis("Category: Library for constructing graphs of media-handling components")
+whatis("Keywords: Library, Multimedia Framework")
+whatis("URL: https://gstreamer.freedesktop.org")
+whatis("Description: Library for constructing graphs of media-handling components")
+
+pushenv("CONDA_DEFAULT_ENV", "gstreamer-1.22.5")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gstreamer/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gstreamer/envs/gstreamer-1.22.5/bin")

20cc80efd81f5bd05c6784d2df9dfa49cbafcad9
Adam Caprez <acaprez2@unl.edu>
2023-09-05 18:43:21 -0500

    Add module file for fastq_utils 0.25.

---
+++ Common/fastq_utils/0.25.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Fastq_utils.
+Version 0.25.2
+]]
+)
+whatis("Name: Fastq_utils")
+whatis("Version: 0.25.2")
+whatis("Category: Genomics, Utilities")
+whatis("Keywords: Genomics, Utilities")
+whatis("URL: https://github.com/nunofonseca/fastq_utils")
+whatis("Description: Validation and manipulation of FASTQ files, scRNA-seq barcode pre-processing and UMI quantification.")
+
+pushenv("CONDA_DEFAULT_ENV", "fastq_utils-0.25.2")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/fastq_utils/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/fastq_utils/envs/fastq_utils-0.25.2/bin")

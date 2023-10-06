e77cb5f5ebd4ce51835aae8a1bf95862f4f2191c
Adam Caprez <acaprez2@unl.edu>
2023-10-05 19:21:23 -0500

    Add module file for code-server 4.17.

---
+++ Common/code-server/4.17.lua
@@ -0,0 +1,15 @@
+help(
+[[
+This module loads code-server
+Version 4.17.1
+]]
+)
+
+whatis("Name: code-server")
+whatis("Version: 4.17.1")
+whatis("Category: Development, Programming")
+whatis("Keywords: Development, Programming")
+whatis("URL: https://coder.com/docs/code-server/latest")
+whatis("Description: VS Code in the browser.")
+
+prepend_path("PATH", "/util/opt/code-server/4.17/bin)

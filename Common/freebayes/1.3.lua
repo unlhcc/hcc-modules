a6bcd19f3e166abdcc37ee79686c1c60cf1ef6fe
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-10-12 10:47:45 -0500

    Update freebayes version

--- Common/freebayes/1.3.lua
+++ Common/freebayes/1.3.lua
@@ -1,16 +1,16 @@
 help(
 [[
 This module loads Freebayes.
-Version 1.3.1
+Version 1.3.7
 ]]
 )
 whatis("Name: Freebayes")
-whatis("Version: 1.3.1")
+whatis("Version: 1.3.7")
 whatis("Category: Biology, Variant, Polymorphism, Bayes, Genotype")
 whatis("Keywords: biology, genotype, bayes")
 whatis("URL: https://github.com/ekg/freebayes")
 whatis("Description: Bayesian haplotype-based polymorphism discovery and genotyping")
 
-pushenv("CONDA_DEFAULT_ENV", "freebayes-1.3.1")
+pushenv("CONDA_DEFAULT_ENV", "freebayes-1.3.7")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/freebayes/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/freebayes/envs/freebayes-1.3.1/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/freebayes/envs/freebayes-1.3.7/bin")

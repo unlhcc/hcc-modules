4d7e5a22980787e89403cf742f06c01a2493ee5b
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-11-10 13:03:59 -0600

    Fix description

--- Common/phyloflash/3.4.lua
+++ Common/phyloflash/3.4.lua
@@ -7,14 +7,13 @@ The environment variable PHYLOFLASH_DBHOME is set and
 points to the generated database location.
 ]]
 )
+
 whatis("Name: Phyloflash")
 whatis("Version: 3.4")
 whatis("Category: Phylogeny, Metagenomics")
 whatis("Keywords: Phylogeny, Metagenomics")
 whatis("URL: https://github.com/HRGV/phyloFlash")
-whatis("Description: phyloFlash is a pipeline to rapidly reconstruct the SSU rRNAs and explore
-phylogenetic composition of an illumina (meta)genomic dataset.
-")
+whatis("Description: phyloFlash is a pipeline to rapidly reconstruct the SSU rRNAs and explore phylogenetic composition of an illumina (meta)genomic dataset.")
 
 pushenv("CONDA_DEFAULT_ENV", "phyloflash-3.4")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/phyloflash/envs")

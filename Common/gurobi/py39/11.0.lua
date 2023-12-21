5a4520a7e7c7f24149040633a229a0a4a38a7df0
Adam Caprez <acaprez2@unl.edu>
2023-12-20 18:51:32 -0600

    Add module files for gurobi 11.0.

---
+++ Common/gurobi/py39/11.0.lua
@@ -0,0 +1,24 @@
+help(
+[[
+This module loads Gurobi.
+Version 11.0.0
+]]
+)
+whatis("Name: Gurobi")
+whatis("Version: 11.0.0")
+whatis("Category: Mathematics, Optimization")
+whatis("Keywords: Mathematics, Optimization")
+whatis("URL: http://www.gurobi.com")
+whatis("Description: The Gurobi Optimizer is the state-of-the-art math programming solver able to handle all major problem types.")
+
+pushenv("CONDA_DEFAULT_ENV", "gurobi-11.0.0-py39")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-11.0.0-py39/bin")
+prepend_path("LD_LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-11.0.0-py39/lib")
+setenv("GUROBI_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-11.0.0-py39")
+setenv("GUROBI_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-11.0.0-py39")
+setenv("GRB_LICENSE_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/gurobi.lic")
+prepend_path("MATLABPATH","/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-11.0.0-py39/share/gurobi-matlab-11.0.0-1/matlab")
+setenv("LAZYLPSOLVERLIBS_GUROBI_LIB", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/envs/gurobi-11.0.0-py39/lib/libgurobi110.so")
+
+family("python")

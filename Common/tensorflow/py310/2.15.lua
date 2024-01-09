4f954da4431792e962ab7db2a7415018e8c76a1b
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-01-09 11:40:10 -0600

    Add module for tf cpu

---
+++ Common/tensorflow/py310/2.15.lua
@@ -0,0 +1,19 @@
+help (
+[[
+This module loads tensorflow.
+Version 2.15.0
+]])
+
+whatis("Name: tensorflow")
+whatis("Version: 2.15.0")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine Learning, Mathematics")
+whatis("Description: TensorFlow is an open source software library for numerical computation using data flow graphs.")
+whatis("URL: https://www.tensorflow.org")
+
+pushenv("CONDA_DEFAULT_ENV", "tensorflow-2.15.0-py310")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow/envs/tensorflow-2.15.0-py310/bin")
+setenv("PYTHONNOUSERSITE", "1")
+
+family("python")

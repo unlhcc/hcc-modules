a6b9fe17f122a239d9f4e0f9d22db752ccdb1b7c
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-01-17 17:30:42 -0600

    Update htseq version

--- Common/htseq/py310/2.0.lua
+++ Common/htseq/py310/2.0.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads HTSeq.
-Version 2.0.2
+Version 2.0.5
 ]]
 )
 
 whatis("Name: HTSeq")
-whatis("Version: 2.0.2")
+whatis("Version: 2.0.5")
 whatis("Category: computational biology, genomics")
 whatis("Keywords: Biology, Genomics, High-throughput Sequencing")
 whatis("URL: https://github.com/simon-anders/htseq")
 whatis("Description: HTSeq is a Python library to facilitate processing and analysis of data from high-throughput sequencing (HTS) experiments.")
 
-pushenv("CONDA_DEFAULT_ENV", "htseq-2.0.2-py310")
+pushenv("CONDA_DEFAULT_ENV", "htseq-2.0.5-py310")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/htseq/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/htseq/envs/htseq-2.0.2-py310/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/htseq/envs/htseq-2.0.5-py310/bin")

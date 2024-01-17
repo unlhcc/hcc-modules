a06ca69ddb3d947e8d913cb00f1601b82745d68c
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-01-17 09:48:16 -0600

    Update gmap version

---
+++ Common/gmap/2023.10.10.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads GMAP and GSNAP.
+Version 2023.10.10
+]])
+
+whatis("Name: GMAP/GSNAP")
+whatis("Version: 2023.10.10")
+whatis("Category: computational biology, sequencing, alignment")
+whatis("Keywords: Biology, Genomics, Sequencing, Alignment")
+whatis("Description: GMAP: A Genomic Mapping and Alignment Program for mRNA and EST Sequences, and GSNAP: Genomic Short-read Nucleotide Alignment Program.")
+whatis("URL: http://research-pub.gene.com/gmap")
+
+pushenv("CONDA_DEFAULT_ENV", "gmap-2023.10.10")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gmap/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/gmap/envs/gmap-2023.10.10/bin")

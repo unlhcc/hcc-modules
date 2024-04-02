3acaf9f006bc60061ec54e2031a440ee701fc83d
Adam Caprez <acaprez2@unl.edu>
2024-04-02 17:50:32 -0500

    Add vars needed for rstudio.

--- Common/r-asreml/r43/4.2.lua
+++ Common/r-asreml/r43/4.2.lua
@@ -13,8 +13,11 @@ whatis("URL: https://www.vsni.co.uk/software/asreml-r")
 whatis("Description: ASReml-R is a statistical package that fits linear mixed models using Residual Maximum Likelihood (REML) in the R environment.")
 
 pushenv("CONDA_DEFAULT_ENV","r-asreml-4.2-r43")
-prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/r-asreml/envs/r-asreml-4.2-r43/bin",priority=100}
+prepend_path("PATH","/util/opt/anaconda/deployed-conda-envs/packages/r-asreml/envs/r-asreml-4.2-r43/bin")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/r-asreml/envs")
 setenv("vsni_LICENSE", "/util/opt/anaconda/deployed-conda-envs/packages/r-asreml/envs/enterprise_licenses/y5kFDXoTR6DHLc8JE.lic")
+prepend_path("LD_LIBRARY_PATH","/util/opt/anaconda/deployed-conda-envs/packages/r-asreml/envs/r-asreml-4.2-r43/lib")
+prepend_path("LD_LIBRARY_PATH","/util/opt/anaconda/deployed-conda-envs/packages/r-asreml/envs/r-asreml-4.2-r43/lib/R/lib")
+setenv("RSTUDIO_WHICH_R","/util/opt/anaconda/deployed-conda-envs/packages/r-asreml/envs/r-asreml-4.2-r43/bin/R")
 
 family("R")

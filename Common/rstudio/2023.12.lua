c0595a29e6ac1137b57a006d0d8bf7940e4a1249
Adam Caprez <acaprez2@unl.edu>
2024-04-02 17:53:21 -0500

    Add module file for rstudio 2023.12.

---
+++ Common/rstudio/2023.12.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads RStudio.
+Version 2023.12
+
+NOTE: You must load an R module as well!
+]]
+)
+whatis("Name: RStudio")
+whatis("Version: 2023.12")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://posit.co/products/open-source/rstudio/")
+whatis("Description: RStudio is an integrated development environment (IDE) for R and Python.")
+
+prepend_path("PATH", "/util/opt/rstudio/rstudio-2023.12.1+402")

7895a131543568cdbd1607fd31674bf132845120
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-04-02 17:59:03 -0500

    Add module file for DSSP

---
+++ Common/dssp/4.4.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads DSSP.
+Version 4.4.7
+]]
+)
+
+whatis("Name: DSSP")
+whatis("Version: 4.4.7")
+whatis("Category: computational biology,PDB")
+whatis("Keywords: PDB,mmCIF,secondary structure")
+whatis("URL: https://swift.cmbi.umcn.nl/gv/dssp/")
+whatis("Description: Application to assign secondary structure to proteins.")
+
+pushenv("CONDA_DEFAULT_ENV", "dssp-4.4.7")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dssp/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dssp/envs/dssp-4.4.7/bin")

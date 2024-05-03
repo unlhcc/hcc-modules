edc8c8c8416df2dff86989dfdd125b105dbdbf2d
Adam Caprez <acaprez2@unl.edu>
2024-05-03 18:55:11 -0500

    Add module file for xcrysen 1.6.

---
+++ Common/xcrysden/1.6.lua
@@ -0,0 +1,15 @@
+help(
+[[
+This module loads Xcrysden.
+Version 1.6.2
+]]
+)
+whatis("Name: Xcrysden")
+whatis("Version: 1.6.2")
+whatis("Category: application, physics, visualization")
+whatis("Keywords: Application, Physics, Visualization")
+whatis("URL: http://www.xcrysden.org/XCrySDen.html")
+whatis("Description: XCrySDen is a crystalline and molecular structure visualisation program aiming at display of isosurfaces and contours, which can be superimposed on crystalline structures and interactively rotated and manipulated.")
+
+setenv("XCRYSDEN_TOPDIR", "/util/opt/xcrysden/1.6")
+prepend_path("PATH", "/util/opt/xcrysden/1.6/bin")

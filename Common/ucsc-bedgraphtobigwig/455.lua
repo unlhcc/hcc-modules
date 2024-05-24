ee64924d6dcb81e366624e1caf757fc035b139ea
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-05-24 16:05:12 -0500

    Add module file for ucsc-bedgraphtobigwig

---
+++ Common/ucsc-bedgraphtobigwig/455.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads ucsc-bedgraphtobigwig.
+Version 455
+]]
+)
+whatis("Name: ucsc-bedgraphtobigwig")
+whatis("Version: 455")
+whatis("Category: computational biology")
+whatis("Keywords: computational biology, bedGraph, bigWig")
+whatis("URL: http://hgdownload.cse.ucsc.edu/admin/exe/")
+whatis("Description: Convert a bedGraph file to bigWig format.")
+
+pushenv("CONDA_DEFAULT_ENV", "ucsc-bedgraphtobigwig-455")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-bedgraphtobigwig/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ucsc-bedgraphtobigwig/envs/ucsc-bedgraphtobigwig-455/bin")

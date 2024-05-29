05b91be8923f344310e25bdc907a7ec7fbeeb0dd
Adam Caprez <acaprez2@unl.edu>
2024-05-29 21:43:42 +0000

    Add module file for mosdepth 0.3.8.

---
+++ Common/mosdepth/0.3.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads Mosdepth.
+Version 0.3.8
+]]
+)
+
+whatis("Name: Mosdepth")
+whatis("Version: 0.3.8")
+whatis("Category: computational biology")
+whatis("Keywords: BAM, CRAM, WGS, exome, targeted sequencing")
+whatis("URL: https://github.com/brentp/mosdepth")
+whatis("Description: Fast BAM/CRAM depth calculation for WGS, exome, or targetted sequencing.")
+
+pushenv("CONDA_DEFAULT_ENV", "mosdepth-0.3.8")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mosdepth/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/mosdepth/envs/mosdepth-0.3.8/bin")

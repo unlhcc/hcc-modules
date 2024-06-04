3754d574a7efb2197eaa6169669071689ed40b1c
Adam Caprez <acaprez2@unl.edu>
2024-06-04 19:20:36 +0000

    Add module file for vesta 3.5.

---
+++ Common/vesta/3.5.lua
@@ -0,0 +1,15 @@
+help(
+[[
+This module loads VESTA.
+Version 3.5.8
+]]
+)
+
+whatis("Name: VESTA")
+whatis("Version: 3.5.8")
+whatis("Category: visualization, physics")
+whatis("Keywords: visualization, physics")
+whatis("URL: https://jp-minerals.org/vesta/en/")
+whatis("Description: VESTA is a 3D visualization program for structural models, volumetric data such as electron/nuclear densities, and crystal morphologies. Some of the novel features of VESTA are listed below.")
+
+prepend_path("PATH", "/util/opt/vesta/3.5")

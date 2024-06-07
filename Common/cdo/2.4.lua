b12fd5f941379f1e248917d4e04060204c015a7a
Cloud User <almalinux@alma86-ivar.novalocal>
2024-06-07 15:30:34 +0000

    Added cdo-2.4 updated cdo-1.9.7.1

--- Common/cdo/2.4.lua
+++ Common/cdo/2.4.lua
@@ -1,17 +1,17 @@
 help(
 [[
 This module loads Cdo.
-Version 1.9.7.1
+Version 2.4.0
 ]]
 )
 whatis("Name: Cdo")
-whatis("Version: 1.9.7.1")
+whatis("Version: 2.4.0")
 whatis("Category: Utilities, Geoscience")
 whatis("Keywords: Utilities, Geoscience")
 whatis("URL: https://code.mpimet.mpg.de/projects/cdo/")
 whatis("Description: CLI tools to manipulate and analyse Climate and NWP model Data")
 
-pushenv("CONDA_DEFAULT_ENV", "cdo-1.9.7.1")
+pushenv("CONDA_DEFAULT_ENV", "cdo-2.4.0")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cdo/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cdo/envs/cdo-1.9.7.1/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/cdo/envs/cdo-2.4.0/bin")
 family("python")

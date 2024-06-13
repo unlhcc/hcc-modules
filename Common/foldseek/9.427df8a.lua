59b6045d5f826b36afa545e06c6446cd5aeeff39
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-06-13 00:33:03 -0500

    Add module file for foldseek

---
+++ Common/foldseek/9.427df8a.lua
@@ -0,0 +1,16 @@
+help(
+[[
+This module loads Foldseek.
+Version 9.427df8a
+]]
+)
+whatis("Name: Foldseek")
+whatis("Version: 9.427df8a")
+whatis("Category: computational bioinformatics")
+whatis("Keywords: bioinformatics, clustering, protein-structure, alignments")
+whatis("URL: https://github.com/steineggerlab/foldseek")
+whatis("Description: Foldseek: fast and accurate protein structure search")
+
+pushenv("CONDA_DEFAULT_ENV", "foldseek-9.427df8a")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/foldseek/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/foldseek/envs/foldseek-9.427df8a/bin")

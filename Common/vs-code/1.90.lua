f6fed6c5fa96a499eda0c5c0a6a4828e4e5849ed
Adam Caprez <acaprez2@unl.edu>
2024-06-20 22:18:15 +0000

    Add module file for vs-code 1.90.

---
+++ Common/vs-code/1.90.lua
@@ -0,0 +1,21 @@
+help(
+[[
+This module loads Visual Studio Code
+Version 1.90.2
+]]
+)
+
+whatis("Name: vs-code")
+whatis("Version: 4.89.1")
+whatis("Category: Development, Programming")
+whatis("Keywords: Development, Programming")
+whatis("URL: https://code.visualstudio.com/")
+whatis("Description: Visual Studio Code")
+
+prepend_path("PATH", "/util/opt/visual-studio-code/1.90/bin")
+
+if mode() == "load" then
+  LmodMessage("NOTE: This module requires a graphical interface, such as the Open OnDemand virtual desktop.")
+  LmodMessage("      To start VS Code, run the 'code' command.")
+end
+

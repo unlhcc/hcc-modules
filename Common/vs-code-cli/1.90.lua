62dc37fd415bb44665195450cbd12d495b5a6c72
Adam Caprez <acaprez2@unl.edu>
2024-06-21 21:53:21 +0000

    Add vs-code-cli module and fix version.

---
+++ Common/vs-code-cli/1.90.lua
@@ -0,0 +1,15 @@
+help(
+[[
+This module loads Visual Studio Code CLI
+Version 1.90.2
+]]
+)
+
+whatis("Name: vs-code-cli")
+whatis("Version: 1.90.2")
+whatis("Category: Development, Programming")
+whatis("Keywords: Development, Programming")
+whatis("URL: https://code.visualstudio.com/")
+whatis("Description: Visual Studio Code")
+
+prepend_path("PATH", "/util/opt/visual-studio-code/1.90-cli/bin")
--- Common/vs-code/1.90.lua
+++ Common/vs-code/1.90.lua
@@ -6,7 +6,7 @@ Version 1.90.2
 )
 
 whatis("Name: vs-code")
-whatis("Version: 4.89.1")
+whatis("Version: 1.90.2")
 whatis("Category: Development, Programming")
 whatis("Keywords: Development, Programming")
 whatis("URL: https://code.visualstudio.com/")

b5e57ce961e16bff07353abd0a853a0824aa444f
Adam Caprez <acaprez2@unl.edu>
2024-06-26 20:01:46 +0000

    Add module files for dorado/dorado-gpu 0.7.

---
+++ Common/dorado/0.7.lua
@@ -0,0 +1,17 @@
+help(
+[[
+This module loads dorado.
+Version 0.7.2
+]]
+)
+
+whatis("Name: dorado")
+whatis("Version: 0.7.2")
+whatis("Category: bioinformatics, ONT")
+whatis("Keywords: bioinformatics, ONT, basecalling")
+whatis("URL: https://github.com/nanoporetech/dorado")
+whatis("Description: Dorado is a high-performance, easy-to-use, open source basecaller for Oxford Nanopore reads.")
+
+pushenv("CONDA_DEFAULT_ENV", "dorado-0.7.2")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dorado/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/dorado/envs/dorado-0.7.2/bin")

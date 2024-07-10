fae8b9a291be80bac3e99f10552e3c644546ec3c
Adam Caprez <acaprez2@unl.edu>
2024-07-10 23:17:20 +0000

    Add na^H^H reminder message to java modules.

--- Common/java/1.8.lua
+++ Common/java/1.8.lua
@@ -16,3 +16,12 @@ prepend_path("PATH"       ,"/util/opt/java/1.8/bin")
 setenv("JAVA_HOME"	  ,"/util/opt/java/1.8")
 
 family("java")
+
+if mode() == "load" then
+  LmodMessage("---------------------------------| Important Notice Regarding Java |--------------------------------------")
+  LmodMessage("Due to licensing terms, Oracle Java(tm) may no longer be used on University systems past August 1st, 2024.")
+  LmodMessage("Please adjust your workflows to use an alternative such as openjdk or corretto-jdk.")
+  LmodMessage("Oracle Java(tm) will be removed from all HCC systems no later than August 1st 2024, at which time")
+  LmodMessage("the java modules will automatically redirect to openjdk.")
+  LmodMessage("----------------------------------------------------------------------------------------------------------")
+end

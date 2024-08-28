6549922246ea7aeaccab127814536617b4669b6a
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-08-28 18:22:04 -0500

    Update VESTA module

--- Common/vesta/3.5.lua
+++ Common/vesta/3.5.lua
@@ -5,11 +5,17 @@ Version 3.5.8
 ]]
 )
 
+if mode() == "load" then
+  LmodMessage("NOTE: This module requires a graphical environment, such as the Open OnDemand virtual desktop."
+)
+  LmodMessage("      To start VESTA, run the 'VESTA' command.")
+end
+
 whatis("Name: VESTA")
 whatis("Version: 3.5.8")
 whatis("Category: visualization, physics")
-whatis("Keywords: visualization, physics")
+whatis("Keywords: visualization, physics, models")
 whatis("URL: https://jp-minerals.org/vesta/en/")
-whatis("Description: VESTA is a 3D visualization program for structural models, volumetric data such as electron/nuclear densities, and crystal morphologies. Some of the novel features of VESTA are listed below.")
+whatis("Description: VESTA is a 3D visualization program for structural models, volumetric data such as electron/nuclear densities, and crystal morphologies.")
 
 prepend_path("PATH", "/util/opt/vesta/3.5")

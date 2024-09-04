63e65074c0edbbd81552a0526fdd775ce67db3f4
Cloud User <almalinux@alma86-ivar.novalocal>
2024-09-04 18:45:48 +0000

    Added compass-cplex-0.2022.04.02

---
+++ Common/compass-cplex/2022.04.02.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads Compass-cplex.
+Version 0.2022.04.02
+]]
+)
+whatis("Name: Compass-cplex")
+whatis("Version: 0.2022.04.02")
+whatis("Category: bioinformatics")
+whatis("Keywords: bioinformatics, in silico algorithm")
+whatis("URL: https://github.com/YosefLab/Compass")
+whatis("Description: In-Silico Modeling of Metabolic Heterogeneity using Single-Cell Transcriptomes.")
+
+load("ibm-ilog-cplex/20.10")
+
+pushenv("CONDA_DEFAULT_ENV", "compass-cplex-0.2022.04.02")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/compass-cplex/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/compass-cplex/envs/compass-cplex-0.2022.04.02/bin")
+family("python")

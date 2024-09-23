76aa522cc06aa6f45f235adfc34e005fd48775eb
Natasha Pavlovikj <npavlovikj2@unl.edu>
2024-09-23 11:59:11 -0500

    Update ufcg version

--- Common/ufcg/1.0.lua
+++ Common/ufcg/1.0.lua
@@ -1,19 +1,19 @@
 help(
 [[
 This module loads UFCG.
-Version 1.0.5
+Version 1.0.6
 ]]
 )
 
 whatis("Name: UFCG")
-whatis("Version: 1.0.5")
+whatis("Version: 1.0.6")
 whatis("Category: computational biology")
 whatis("Keywords: computational biology, annotation, Fungi")
 whatis("URL: https://ufcg.steineggerlab.com")
 whatis("Description: UFCG pipeline provides methods for a genome-wide taxonomic profiling and annotation of your own biological sequences of Fungi.")
 
-pushenv("CONDA_DEFAULT_ENV", "ufcg-1.0.5")
+pushenv("CONDA_DEFAULT_ENV", "ufcg-1.0.6")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ufcg/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ufcg/envs/ufcg-1.0.5/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ufcg/envs/ufcg-1.0.6/bin")
 
 family("java")

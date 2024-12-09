bea9f86dc9dad0720e40d4aab877cd495fd2d445
Adam Caprez <acaprez2@unl.edu>
2024-12-09 21:48:36 +0000

    Add perl family to braker3/3.0 module.

--- Common/braker3/3.0.lua
+++ Common/braker3/3.0.lua
@@ -14,4 +14,4 @@ whatis("Description: BRAKER3 is the latest pipeline in the BRAKER suite")
 pushenv("CONDA_DEFAULT_ENV", "braker3-3.0.8")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/braker3/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/braker3/envs/braker3-3.0.8/bin")
-
+family("perl")

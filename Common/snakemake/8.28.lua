9699411050c81b3ddb944f62b670d47a8180a403
Mohammed Tanash <tanash@login1.swan.hcc.unl.edu>
2025-02-25 18:03:50 -0600

    updated snakemake-8.28.0 module file

--- Common/snakemake/8.28.lua
+++ Common/snakemake/8.28.lua
@@ -11,6 +11,6 @@ whatis("Keywords: Utility, Workflow")
 whatis("URL: https://snakemake.readthedocs.io")
 whatis("Description: A popular workflow management system aiming at full in-silico reproducibility.")
 
-pushenv("CONDA_DEFAULT_ENV", "snakemake-6.4.1")
+pushenv("CONDA_DEFAULT_ENV", "snakemake-8.28.0")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snakemake/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/snakemake/envs/snakemake-8.28.0/bin")

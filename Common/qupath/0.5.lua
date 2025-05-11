dc2e6d19e60662cc40532e5bb61e22f5b374e3a6
Natasha Pavlovikj <npavlovikj2@unl.edu>
2025-05-11 00:32:12 -0500

    Add module file for QuPath

---
+++ Common/qupath/0.5.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module loads QuPath.
+Version 0.5.1
+]]
+)
+
+whatis("Name: QuPath")
+whatis("Version: 0.5.1")
+whatis("Category: bioimaging")
+whatis("Keywords: bioimaging,pathology,slide image analysis")
+whatis("URL: https://qupath.github.io/")
+whatis("Description: QuPath is open source software for bioimage analysis.")
+
+pushenv("CONDA_DEFAULT_ENV", "qupath-0.5.1")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/qupath/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/qupath/envs/qupath-0.5.1/bin")
+
+family("java")

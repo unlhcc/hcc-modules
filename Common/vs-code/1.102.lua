30e2be88d89f8c6f50b266a341f489087e669352
Adam Caprez <acaprez2@unl.edu>
2025-07-28 20:03:33 +0000

    Add modules for vs-code/vs-code-cli 1.102.

---
+++ Common/vs-code/1.102.lua
@@ -0,0 +1,21 @@
+help(
+[[
+This module loads Visual Studio Code
+Version 1.102.2
+]]
+)
+
+whatis("Name: vs-code")
+whatis("Version: 1.102.2")
+whatis("Category: Development, Programming")
+whatis("Keywords: Development, Programming")
+whatis("URL: https://code.visualstudio.com/")
+whatis("Description: Visual Studio Code")
+
+prepend_path("PATH", "/util/opt/visual-studio-code/1.102/bin")
+
+if mode() == "load" then
+  LmodMessage("NOTE: This module requires a graphical environment, such as the Open OnDemand virtual desktop.")
+  LmodMessage("      To start VS Code, run the 'code' command.")
+end
+

524527da3fe154c0bbdf4c3964a137ef4f5e7612
Natasha Pavlovikj <npavlovikj2@unl.edu>
2025-08-12 23:35:07 -0500

    Add ROCR_VISIBLE_DEVICES for GPU nodes

--- Common/ollama-gpu/0.11.lua
+++ Common/ollama-gpu/0.11.lua
@@ -16,6 +16,7 @@ pushenv("CONDA_DEFAULT_ENV", "ollama-gpu-0.11.4")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ollama-gpu/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ollama-gpu/envs/ollama-gpu-0.11.4/bin")
 setenv("OLLAMA_MODELS",pathJoin(os.getenv("WORK"),".ollama/models"))
+unsetenv("ROCR_VISIBLE_DEVICES")
 setenv("PYTHONNOUSERSITE", "1")
 
 family("python")

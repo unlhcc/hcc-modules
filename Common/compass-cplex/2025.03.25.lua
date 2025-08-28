80dfc7bf70998c2daa5f80d3ed2d3fc0486fcdc6
Natasha Pavlovikj <npavlovikj2@unl.edu>
2025-08-27 23:29:21 -0500

    Fix name

---
+++ Common/compass-cplex/2025.03.25.lua
@@ -0,0 +1,25 @@
+help(
+[[
+This module loads compass-cplex.
+Version 0.2025.03.25
+]]
+)
+
+if mode() == "load" then
+  LmodMessage("------------------------| Help message for Compass module |-------------------------")
+  LmodMessage("NOTE: The first time you run Compass, please set the existing Gurobi license with:")
+  LmodMessage("compass --set-license $GRB_LICENSE_FILE")
+  LmodMessage("------------------------------------------------------------------------------------")
+end
+
+whatis("Name: compass-cplex")
+whatis("Version: 0.2025.03.25")
+whatis("Category: bioinformatics, single-cell")
+whatis("Keywords: bioinformatics, single-cell, transcriptomes")
+whatis("URL: https://github.com/wagnerlab-berkeley/Compass")
+whatis("Description: In-Silico Modeling of Metabolic Heterogeneity using Single-Cell Transcriptomes.")
+
+pushenv("CONDA_DEFAULT_ENV", "compass-cplex-0.2025.03.25")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/compass-cplex/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/compass-cplex/envs/compass-cplex-0.2025.03.25/bin")
+setenv("GRB_LICENSE_FILE", "/util/opt/anaconda/deployed-conda-envs/packages/gurobi/gurobi.lic")

331a4031d4353764845f837f133c21294283d8a6
Cloud User <almalinux@ian-app-test.novalocal>
2025-10-23 19:29:27 +0000

    upgrade ViennaRNA FD# 47887

---
+++ Common/viennarna/2.7.lua
@@ -0,0 +1,17 @@
+help (
+[[
+This module loads ViennaRNA.
+Version 2.7.0
+
+]])
+
+whatis("Name: ViennaRNA")
+whatis("Version: 2.7.0")
+whatis("Category: computational biology, RNA, secondary structure")
+whatis("Keywords: Biology, Structure")
+whatis("URL: https://www.tbi.univie.ac.at/RNA")
+whatis("Description: The ViennaRNA Package consists of a C code library and several stand-alone programs for the prediction and comparison of RNA secondary structures.")
+
+pushenv("CONDA_DEFAULT_ENV","viennarna-2.7.0")
+prepend_path{"PATH","/util/opt/anaconda/deployed-conda-envs/packages/viennarna/envs/viennarna-2.7.0/bin",priority=100}
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/viennarna/envs")

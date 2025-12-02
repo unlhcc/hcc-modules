f9a7b2a56908365e0fd7e44ddea6d316cb5d1bc5
Mohammed Tanash <tanash@login1.swan.hcc.unl.edu>
2025-12-02 14:49:53 -0600

    updated the tobias module file for tobias to use 0.17.2 instead of 0.17.1

--- Common/tobias/py312/0.17.lua
+++ Common/tobias/py312/0.17.lua
@@ -1,19 +1,19 @@
 help(
 [[
 This module loads TOBIAS.
-Version 0.17.1
+Version 0.17.2
 ]]
 )
 
 whatis("Name: Tobias")
-whatis("Version: 0.17.1")
+whatis("Version: 0.17.2")
 whatis("Category: computational biology, ATAC-seq")
 whatis("Keywords: ATAC-seq, transcription factor, Tn5")
 whatis("URL: https://github.molgen.mpg.de/loosolab/TOBIAS")
 whatis("Description: Transcription factor Occupancy prediction By Investigation of ATAC-seq Signal")
 
-pushenv("CONDA_DEFAULT_ENV", "tobias-0.17.1")
+pushenv("CONDA_DEFAULT_ENV", "tobias-0.17.2")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tobias/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tobias/envs/tobias-0.17.1/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tobias/envs/tobias-0.17.2/bin")
 
 family("python")

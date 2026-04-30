e048148b999027ae0e45e9d1812824a212ca3a29
almalinux Cloud User <almalinux@almalinux8-xxy.novalocal>
2026-04-30 22:31:01 +0000

    Add funannotate 1.8.17

--- Common/funannotate/1.8.lua
+++ Common/funannotate/1.8.lua
@@ -5,6 +5,13 @@ Version 1.8.17
 ]]
 )
 
+if mode() == "load" then
+  LmodMessage("-------------------------| Help message for funannotate module |------------------------------")
+  LmodMessage("If you are running GeneMark for a first time, please copy the key to your HOME directory by typing:")
+  LmodMessage("cp $GM_KEY $HOME/.gm_key")
+  LmodMessage("------------------------------------------------------------------------------------------")
+end
+
 whatis("Name: Funannotate")
 whatis("Version: 1.8.17")
 whatis("Category: Bioinformatics")
@@ -18,5 +25,8 @@ prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/funannotat
 
 setenv("FUNANNOTATE_DB", "/work/HCC/BCRF/app_specific/funannotate/1.8.17")
 setenv("AUGUSTUS_CONFIG_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/funannotate/envs/funannotate-1.8.17/config")
+setenv("EVM_HOME", "/util/opt/anaconda/deployed-conda-envs/packages/funannotate/envs/funannotate-1.8.17/opt/evidencemodeler-2.1.0")
+setenv("GENEMARK_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/funannotate/envs/funannotate-1.8.17/bin")
+setenv("GM_KEY", "/util/opt/anaconda/deployed-conda-envs/packages/funannotate/envs/funannotate-1.8.17/bin/gm_key")
 
 family("python")

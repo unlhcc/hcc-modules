8ada4972aa7b4ad3dd087a5e2eea8838d12e403f
almalinux Cloud User <almalinux@almalinux8-xxy.novalocal>
2026-05-29 19:29:09 +0000

    Update beagle-lib to verion 4.0.1 with CUDA support.

--- Common/beagle-lib/4.0.lua
+++ Common/beagle-lib/4.0.lua
@@ -1,19 +1,19 @@
 help(
 [[
 This module loads beagle-lib.
-Version 4.0.0
+Version 4.0.1 includes CUDA support.
 ]]
 )
 
 whatis("Name: beagle-lib")
-whatis("Version: 4.0.0")
+whatis("Version: 4.0.1")
 whatis("Category: computational biology, library")
-whatis("Keywords: computational biology, phylogenetics, library")
+whatis("Keywords: computational biology, phylogenetics, library, CUDA")
 whatis("URL: https://github.com/beagle-dev/beagle-lib")
 whatis("Description: general purpose library for evaluating the likelihood of sequence evolution on trees")
 
-pushenv("CONDA_DEFAULT_ENV", "beagle-lib-4.0.0")
+pushenv("CONDA_DEFAULT_ENV", "beagle-lib-4.0.1")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/beagle-lib/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/beagle-lib/envs/beagle-lib-4.0.0/bin")
-prepend_path("LD_LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/beagle-lib/envs/beagle-lib-4.0.0/lib")
-prepend_path("LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/beagle-lib/envs/beagle-lib-4.0.0/lib")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/beagle-lib/envs/beagle-lib-4.0.1/bin")
+prepend_path("LD_LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/beagle-lib/envs/beagle-lib-4.0.1/lib")
+prepend_path("LIBRARY_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/beagle-lib/envs/beagle-lib-4.0.1/lib")

e1d02552a94985aa8867aacb673dbc38ad96c8bc
Natasha Pavlovikj <npavlovikj@unl.edu>
2026-06-03 20:57:56 +0000

    Matlab path update

--- Common/matlab/r2025b.lua
+++ Common/matlab/r2025b.lua
@@ -7,8 +7,11 @@ help(help_message,"\n")
 whatis("Name: MATLAB r2025b")
 whatis("Keywords: Application, Engineering, Math")
 
-local HOME = os.getenv("HOME")
-setenv("MATLAB_PREFDIR", HOME .. "/.matlab/R2025b/prefs")
+local d=pathJoin(os.getenv("HOME"),".matlab/R2025b/prefs")
+if (not isDir(d)) then
+  lfs.mkdir(d)
+end
+setenv("MATLAB_PREFDIR", d)
 setenv("TZ", "America/Chicago") --fix the TimeZone warning message
 
 prepend_path("PATH","/util/opt/matlab/r2025b/bin")

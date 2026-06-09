43e845c19ed89fce96f6916ebfc5759e0f97175b
Natasha Pavlovikj <npavlovikj@unl.edu>
2026-06-09 15:30:11 +0000

    Remove MATLAB_PREFDIR, Matlab sets it to $HOME/.matlab/$VERSION by default

--- Common/matlab/r2025b.lua
+++ Common/matlab/r2025b.lua
@@ -7,11 +7,6 @@ help(help_message,"\n")
 whatis("Name: MATLAB r2025b")
 whatis("Keywords: Application, Engineering, Math")
 
-local d=pathJoin(os.getenv("HOME"),".matlab/R2025b/prefs")
-if (not isDir(d)) then
-  lfs.mkdir(d)
-end
-setenv("MATLAB_PREFDIR", d)
 setenv("TZ", "America/Chicago") --fix the TimeZone warning message
 
 prepend_path("PATH","/util/opt/matlab/r2025b/bin")

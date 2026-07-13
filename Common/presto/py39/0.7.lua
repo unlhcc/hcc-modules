bfe49851848bc36722ae5659325cd39b53d01552
almalinux Cloud User <almalinux@almalinux8-xxy.novalocal>
2026-07-13 19:27:51 +0000

    Update presto to version presto-0.7.9-py39

--- Common/presto/py39/0.7.lua
+++ Common/presto/py39/0.7.lua
@@ -1,20 +1,20 @@
 help(
 [[
 This module loads pRESTO.
-Version 0.7.4
+Version 0.7.9
 ]]
 )
 
 whatis("Name: pRESTO")
-whatis("Version: 0.7.4")
+whatis("Version: 0.7.9")
 whatis("Category: computational biology,sequencing")
 whatis("Keywords: computational biology,sequencing,alignment")
 whatis("URL: http://presto.readthedocs.io")
 whatis("Description: A bioinformatics toolkit for processing high-throughput lymphocyte receptor sequencing data.")
 
-pushenv("CONDA_DEFAULT_ENV", "presto-0.7.4-py39")
+pushenv("CONDA_DEFAULT_ENV", "presto-0.7.9-py39")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/presto/envs")
-prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/presto/envs/presto-0.7.4-py39/bin")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/presto/envs/presto-0.7.9-py39/bin")
 setenv("PYTHONNOUSERSITE", "1")
 
 family("python")

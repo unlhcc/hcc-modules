708b8029c7765645ce94c0bee9bbb1cbf22f0190
Mohammed Tanash <tanash@login1.swan.hcc.unl.edu>
2026-08-07 16:51:04 -0500

    Update module files  for af_cluster-2025.10.07.lua

--- Common/af_cluster-gpu/2025.10.07.lua
+++ Common/af_cluster-gpu/2025.10.07.lua
@@ -1,11 +1,11 @@
 help(
 [[
 This module loads Af_cluster GPU.
-Version 0.2025.10.07
+Version 2025.10.07
 ]]
 )
 whatis("Name: Af_cluster GPU")
-whatis("Version: 0.2025.10.07")
+whatis("Version: 2025.10.07")
 whatis("Category: bioinformatics, structural biology")
 whatis("Keywords: protein structure, alphafold, colabfold, conformational ensembles, MSA clustering")
 whatis("URL: https://github.com/HWaymentSteele/AF_Cluster")
@@ -14,3 +14,5 @@ whatis("Description: Predict multiple protein conformations via MSA sequence clu
 pushenv("CONDA_DEFAULT_ENV", "af_cluster-gpu-0.2025.10.07")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/af_cluster-gpu/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/af_cluster-gpu/envs/af_cluster-gpu-0.2025.10.07/bin")
+
+family("python")

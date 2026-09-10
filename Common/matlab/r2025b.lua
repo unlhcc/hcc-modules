adaf60a43125f26bc2ef6c6aebf93c9b6845037f
Natasha Pavlovikj <npavlovikj@unl.edu>
2026-09-10 00:25:55 +0000

    Update MATLAB 2025

--- Common/matlab/r2025b.lua
+++ Common/matlab/r2025b.lua
@@ -11,3 +11,10 @@ setenv("TZ", "America/Chicago") --fix the TimeZone warning message
 
 prepend_path("PATH","/util/opt/matlab/r2025b/bin")
 setenv("MATLAB_ROOT","/util/opt/matlab/r2025b")
+
+--Parallel MATLAB
+local SUPPORT_PACKAGES = "/util/opt/matlab/support_packages"
+local MATLAB_CLUSTER_PROFILES_LOCATION = pathJoin(SUPPORT_PACKAGES, "matlab_parallel_server/scripts")
+setenv("MATLAB_CLUSTER_PROFILES_LOCATION", MATLAB_CLUSTER_PROFILES_LOCATION)
+append_path("PATH", pathJoin(SUPPORT_PACKAGES, "matlab_parallel_server/bin"))
+prepend_path("MATLABPATH", MATLAB_CLUSTER_PROFILES_LOCATION)
